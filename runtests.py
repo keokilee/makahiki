7a8391bf1a36c071d13916f6ca9155e709a497c4
George Lee <keoki.lee@gmail.com>
2010-03-05 14:18:30 -1000

    Removing test from list of failing tests.

--- runtests.py
+++ runtests.py
@@ -1,6 +1,6 @@
 import os
 
-testapps = ["basic_profiles", "tribes"]
+testapps = ["basic_profiles"]
 command = "python manage.py test"
 print "**Executing the following tests: **"
 for app in testapps:
