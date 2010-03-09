30300ed82694e7e19bc4f6258b504c59bc6640b9
George Lee <keoki.lee@gmail.com>
2010-03-08 17:14:41 -1000

    Modified runtests.py so that it runs tests for all apps in the apps directory. Updated settings accordingly.

--- runtests.py
+++ runtests.py
@@ -1,10 +1,20 @@
 import os
 
-testapps = ["basic_profiles"]
+items = os.listdir("apps/")
 command = "python manage.py test"
 print "**Executing the following tests: **"
-for app in testapps:
-  print app
-  command += " " + app
-
+for item in items:
+  if os.path.isdir("apps/" + item):
+    print item
+    command += " " + item
+  
 os.system(command)
+
+# testapps = ["basic_profiles"]
+# command = "python manage.py test"
+# print "**Executing the following tests: **"
+# for app in testapps:
+#   print app
+#   command += " " + app
+# 
+# os.system(command)
