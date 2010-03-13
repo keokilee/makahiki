5677f071aff48a419146ba71334d7bb17cfc82da
George Lee <keoki.lee@gmail.com>
2010-03-12 18:15:49 -1000

    Updated user profile and added in commitments.  Refactored the models in the activities app.

---
+++ apps/kukui_cup_profile/forms.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django import forms
+
+from kukui_cup_profile.models import Profile
+
+class ProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        exclude = ('user', 'points')
