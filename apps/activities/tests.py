37e407cc4b30b3723ed16b80867c1ffdee7bcb2e
George Lee <keoki.lee@gmail.com>
2010-04-11 18:20:31 -1000

    Adding some unit tests.

--- apps/activities/tests.py
+++ apps/activities/tests.py
@@ -1,6 +1,45 @@
 import unittest
 
+from django.contrib.auth.models import User
+from kukui_cup_profile.models import Profile
+from activities.models import Activity, ActivityMember
+
 class ActivitiesTestCase(unittest.TestCase):
-  def testBasic(self):
-    foo = "Python"
-    self.assertEquals(foo, "Python")
+  def testApproveAddsPoints(self):
+    """Test for verifying that approving a user awards them points."""
+    user = User.objects.all()[0]
+    points = user.get_profile().points
+    
+    activity = Activity.objects.all()[0]
+    activity_points = activity.point_value
+    
+    activity_member = ActivityMember(user=user, activity=activity)
+    activity_member.approval_status = "approved"
+    activity_member.save()
+    
+    new_points = user.get_profile().points
+    self.assertTrue(new_points - points == activity_points)
+    self.assertTrue(activity_member.awarded)
+    
+  def testUnapproveRemovesPoints(self):
+    """Test that unappoving a user removes their points."""
+    user = User.objects.all()[0]
+    points = user.get_profile().points
+    
+    activity = Activity.objects.all()[0]
+    
+    activity_member = ActivityMember(user=user, activity=activity)
+    activity_member.approval_status = "approved"
+    activity_member.save()
+    
+    activity_member.approval_status = "rejected"
+    activity_member.save()
+    new_points = user.get_profile().points
+    
+    self.assertTrue(points == new_points)
+    self.assertFalse(activity_member.awarded)
+    
+    
+    
+    
+    
