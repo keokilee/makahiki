f9647b9de6a96b266b61888c9416f32701ae8e83
George Lee <keoki.lee@gmail.com>
2010-04-16 16:04:28 -1000

    Added kukui_cup_base app to handle rendering of the home page.

---
+++ apps/kukui_cup_base/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from news.models import Article
+
+def homepage(request):
+  articles = Article.objects.all()[:5]
+  return render_to_response("homepage.html", {
+    "articles": articles,
+  })
+  
