9250fc7146bd62133a831d90d4df1328d633f60e
George Lee <keoki.lee@gmail.com>
2010-04-18 15:56:40 -1000

    Added field for user theme.

--- apps/kukui_cup_profile/models.py
+++ apps/kukui_cup_profile/models.py
@@ -1,15 +1,25 @@
+import os
+
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 
+def _get_available_themes():
+  """Retrieves the available themes from the media folder."""
+  
+  theme_dir = os.path.join(settings.PROJECT_ROOT, "media")
+  # Returns a list of tuples representing the name of the theme and the directory of the theme
+  return ((item.capitalize(), item) for item in os.listdir(theme_dir) 
+                      if os.path.isdir(os.path.join(theme_dir, item)))
+  
 class Profile(models.Model):
-
     user = models.ForeignKey(User, unique=True, verbose_name=_('user'))
     name = models.CharField(_('name'), max_length=50, null=True, blank=True)
     about = models.TextField(_('about'), null=True, blank=True)
     points = models.IntegerField(default=0, editable=False)
+    theme = models.CharField(max_length=255, default="default", choices=_get_available_themes())
     
     def __unicode__(self):
         return self.user.username
