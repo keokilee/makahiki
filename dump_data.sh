059c7562a89fd60de3c1b596d42bf8ad7d8a3113
George Lee <keoki.lee@gmail.com>
2010-05-28 10:38:45 -1000

    Updating to add wattdepot host to settings.  Added floors to dump data shell script.

--- dump_data.sh
+++ dump_data.sh
@@ -1,2 +1,2 @@
-python manage.py dumpdata auth activities kukui_cup_base django_generic_flatblocks gblocks --indent 1 > fixtures/initial_data.json
+python manage.py dumpdata auth activities kukui_cup_base kukui_cup_floors django_generic_flatblocks gblocks --indent 1 > fixtures/initial_data.json
 
