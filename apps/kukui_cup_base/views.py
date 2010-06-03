45944036fc575661944c3486712b9c39d0f0c3e1
George Lee <keoki.lee@gmail.com>
2010-06-02 16:49:15 -1000

    Renaming folder to reflect the Django app.

--- apps/kukui_cup_base/views.py
+++ apps/kukui_cup_base/views.py
@@ -2,12 +2,16 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-from kukui_cup_base.models import Article
+from kukui_cup_base.models import Article, Headline
 
 def homepage(request):
+  # Retrieve latest articles and headlines.
   articles = Article.objects.order_by('-pk')
+  headlines = Headline.objects.order_by('-pk')[:10]
+  
   return render_to_response("homepage.html", {
     "articles": articles,
+    "headlines": headlines,
   }, context_instance = RequestContext(request))
   
 def article(request, item_id, slug=None):
