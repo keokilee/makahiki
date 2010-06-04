3d5deb473e1b296958995abfb486fd360cb44b4b
George Lee <keoki.lee@gmail.com>
2010-06-03 16:40:36 -1000

    Updating gauge and chart to use cached data.

--- media/js/chart.js
+++ media/js/chart.js
@@ -6,33 +6,39 @@ google.setOnLoadCallback(initializeData);
 
 // Once visualization API is loaded, retrieve data and set callbacks to run once retrieved.
 function initializeData() {
-  // Get Overall Dorm Data.
-  var dormURL = 'http://spreadsheets.google.com/tq?key=0An9ynmXUoikYdG94TnRCYjNqNng5MVZYb01SUEFMQVE&range=C10:D13&gid=0';
-  var dormQuery = new google.visualization.Query(dormURL);
+  // Get all of the dorm data from the spreadsheet.
+  var dormDataURL = 'http://spreadsheets.google.com/tq?key=0Av0U6TKHfzXYdG1vUnduR0RVTktyR1ZtNjAtSE9Qbmc&range=A3:F21&gid=0';
+  var dormDataQuery = new google.visualization.Query(dormDataURL);
+  // Update the pie chart once an hour.
+  dormDataQuery.setRefreshInterval(3600);
 
-  // Set a callback to run when the overall dorm data has been retrieved.
-  dormQuery.send(displayDormEnergyData);
+  // Set a callback to run when the dorm data has been retrieved.
+  dormDataQuery.send(displayDormData);
 }
 
 
-// Callback that gets the Overall Dorm DataTable and displays it.
-function displayDormEnergyData(response) {
-// Process errors, if any.
-if (response.isError()) {
-   alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-   return;
-}
-
-// Get the data table, and sort it by energy value.
-var data = response.getDataTable();
-data.sort([{column: 1}]);
-var chart = new google.visualization.BarChart(document.getElementById('dorm_chart_div'));
-chart.draw(data, {backgroundColor: '#F5F3E5',
-                 colors: ['#459E00'],
-                 width: 300,
-                 height: 200,
-                 legend: 'none',
-                 title: 'Dorm Energy Consumption',
-                 hAxis: {minValue: '0', title: 'kWh consumed for last 10 days'}
-               });
+// Callback that gets the dorm data and creates DataViews to display each table individually.
+function displayDormData(response) {
+  // Process errors, if any.
+  if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+  }
+  
+  // Get the dorm data table.
+  var data = response.getDataTable();
+  
+  // Create the view that is just the total dorm energy data.
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0,4]);
+  view.setRows([5,11,17]);
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  chart.draw(view, {backgroundColor: '#F5F3E5',
+                    colors: ['#459E00'],
+                    width: 300, 
+                    height: 200, 
+                    title: 'Energy Usage since 5/25/2010',
+                    legend: 'none',
+                    hAxis: {minValue: '0', title: 'watt-hours'}
+                    });
 }
--- media/js/gauge.js
+++ media/js/gauge.js
@@ -1,176 +1,37 @@
-/*
- * Modified version of the Wattdepot Gauge code used in http://code.google.com/p/wattdepot-ui-googlegadgets/.
- * Mahalo to Jarret Mizo and Paul Galiza.
- */
+// Load the Visualization API and the bar chart package.
+google.load("visualization", "1", {packages:['gauge']});
 
-var gauge = null;
-var options = null;
-var host = null;
-var dataSourceUrl = null;
-var source = null;
-var dataSourceUrlPart1 = "/sources/";
-var dataSourceUrlPart2 = "/sensordata/latest?tq=select%20timePoint%2C%20";
-var dataDisplayed = null;
-var yellowThreshold = null;
-var redThreshold = null;
-var maxThreshold = null;
-var refreshInterval = null;
-var queryDataValue = null;
-var sourcediv = document.getElementById("sourcediv");
-var data_displayed_div = document.getElementById("data_displayed_div");
-var lastcheckdiv = document.getElementById("lastcheckdiv");
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(initializeGauge);
 
-var loadingMsgContainer = document.getElementById('loading');
-if (loadingMsgContainer){
-  loadingMsgContainer.style.display='none';
+// Once visualization API is loaded, retrieve data and set callbacks to run once retrieved.
+function initializeGauge() {
+  // Get all of the dorm data from the spreadsheet.
+  var gaugeURL = 'http://spreadsheets.google.com/tq?key=0Av0U6TKHfzXYdG1vUnduR0RVTktyR1ZtNjAtSE9Qbmc&range=A3:F21&gid=0';
+  var gaugeQuery = new google.visualization.Query(gaugeURL);
+  // Update the gauges every 5 seconds
+  gaugeQuery.setRefreshInterval(5);
+
+  // Set a callback to run when the dorm data has been retrieved.
+  gaugeQuery.send(displayGaugeData);
 }
 
-google.load('visualization', '1', {packages:['gauge']});
-google.setOnLoadCallback(sendQuery);
 
-/**
- * Sets up the data source to be queried.
- */
-function sendQuery() {
-  host = checkTrailingSlash("http://server.wattdepot.org:8184/gviz/");
-  source = "SIM_UH_ILIMA";
-  dataDisplayed = "powerConsumed";
-  yellowThreshold = "4000";
-  redThreshold = "6000";
-  maxThreshold = "8000";
-  refreshInterval = 15000;
-
-  dataSourceUrl = host + dataSourceUrlPart1 + source + dataSourceUrlPart2 + dataDisplayed;
+// Callback that gets the dorm data and creates DataViews to display each table individually.
+function displayGaugeData(response) {
+  // Process errors, if any.
+  if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+  }
   
-  sourcediv.innerHTML = '<p align=\"center\">' + source + '</p>'
-  data_displayed_div.innerHTML= '<p align=\"center\">' + generateDataDisplayed(dataDisplayed) + '</p>';
+  // Get the dorm data table.
+  var data = response.getDataTable();
   
-  gauge = new google.visualization.Gauge(document.getElementById('chartdiv'));
-  
-  setGaugeOptions(yellowThreshold, redThreshold, maxThreshold);
-
-  moveTicker();
+  // Create the view that is just ilima floors.
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0,2]);
+  view.setRows([5, 11, 17]);
+  var chart = new google.visualization.Gauge(document.getElementById('gauge_div'));
+  chart.draw(view, {height: 100, max: 5000, greenFrom: 0, greenTo: 2000, yellowFrom:2001, yellowTo: 4000, redFrom: 4001, redTo: 5000});
 }
-
-/**
- * Query response handler function.
- * Called by the Google Visualization API once the response is received.
- * Takes the query response and formats it as a table.
- */
-function handleDataValueQueryResponse(responseDataValue) {
-  var data = responseDataValue.getDataTable();
-  data.setColumnLabel(1, "Watts");
-  //generateGaugeUnits(data);
-  gauge.draw(data, options);
-}
-
-/**
- * Moves the gadget ticker whenever the gauge refreshes
- * instead of refreshing the whole gauge image.
- */
-function moveTicker() {
-  queryDataValue = new google.visualization.Query(dataSourceUrl);
-  queryDataValue.send(handleDataValueQueryResponse);
-  lastcheckdiv.innerHTML = generateCheckDate();
-  setTimeout("moveTicker()", refreshInterval);
-}
-
-/**
- * Checks if there is a trailing slash in the url to parse to accepted program url.
- * Then returns the string without the trailing slash.
- * Ex. http://server.wattdepot.org:8188/gviz/ returns http://server.wattdepot.org:8188/gviz
- */
-function checkTrailingSlash(url) {
-  var str = url;
-  var char = str.charAt(str.length-1);
-  if (char.match("/") == null){
-    return str;
-  } else {
-    return str.substr(0,str.length-1);
-  }
-}
-
-function generateDataDisplayed(dataDisplayed){
-  var data = dataDisplayed;
-  if (data.match("powerConsumed")) {
-    return "Power Consumed";
-  } else {
-    return "Power Generated";
-  }
-}
-
-function generateCheckDate() {
-  var html = [];
-  var checkDate = new Date();
-  var months = new Array('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec');
-  html.push("<font style = \"font-size:0.8em; font-style: italic\">Last check was at: ");
-  html.push(months[checkDate.getMonth()]);
-  html.push(" " + checkDate.getDate() + ", " + checkDate.getFullYear() + " ");
-  html.push(checkDate.toLocaleTimeString());
-  html.push("</font>");
-  return html.join('');
-}
-
-function setGaugeOptions(yellowThreshold, redThreshold, maxGaugeValue){
-  var yellowValue = yellowThreshold;
-  var redValue = redThreshold;
-  var maxValue = maxGaugeValue;
-
-  if(yellowValue == 0){
-    yellowValue = null;
-  }
-
-  if(redValue == 0){
-    redValue = null;
-  }
-
-  options = {width: 400, height: 120, redFrom: redValue, redTo: maxValue, yellowFrom:yellowValue, yellowTo: redValue, minorTicks: 5, max: maxValue};
-}
-
-/**
- * Since the power generated or consumed is always in watts, this function converts watts to MW, or kW. 
- */
-function generateGaugeUnits(dataTable){
-  var table = dataTable;
-  var watts = table.getValue(0,1);
-  var unit = null;
-  
-  if(watts >= 10000000){
-    unit = "MW";
-    watts = watts/1000000;
-  } else if(watts >= 10000){
-    unit = "kW";
-    watts = watts/1000
-  } else {
-    unit = "Watts";
-  }
-
-  //debugdiv.innerHTML = '<p align=\"center\" style=\"font-weight:bold;font-size:large;\">Cell (0,1):' +watts + '</p>';
-
-  table.setColumnLabel(1, unit);
-  table.setValue(0,1,watts);
-  updateGaugeOptions(unit);
-}
-
-function updateGaugeOptions(unit){
-  var yellowValue = yellowThreshold;
-  var redValue = redThreshold;
-  var maxValue = maxThreshold;
-
-  if(unit.match("MW")){
-    yellowValue = yellowValue/1000000;
-    redValue = redValue/1000000;
-    maxValue = maxValue/1000000;
-  }
-
-  if(unit.match("kW")){
-    yellowValue = yellowValue/1000;
-    redValue = redValue/1000;
-    maxValue = maxValue/1000;
-  }
-
-  options = {width: 400, height: 120, redFrom: redValue, redTo: maxValue, yellowFrom:yellowValue, yellowTo: redValue, minorTicks: 5, max: maxValue};
-}
-
-
-
