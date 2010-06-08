b54bf70c0ba110f4ebe9b779bab957f6036a86c2
George Lee <keoki.lee@gmail.com>
2010-06-07 14:46:02 -1000

    Updating chart javascript.

--- media/js/chart.js
+++ media/js/chart.js
@@ -2,22 +2,25 @@
 google.load("visualization", "1", {packages:['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
-google.setOnLoadCallback(initializeData);
+google.setOnLoadCallback(initializeChart);
 
-// Once visualization API is loaded, retrieve data and set callbacks to run once retrieved.
-function initializeData() {
+/**
+ * Once visualization API is loaded, retrieve data and set callback to run once retrieved.
+ */
+function initializeChart() {
   // Get all of the dorm data from the spreadsheet.
   var dormDataURL = 'http://spreadsheets.google.com/tq?key=0Av0U6TKHfzXYdG1vUnduR0RVTktyR1ZtNjAtSE9Qbmc&range=A3:F21&gid=0';
   var dormDataQuery = new google.visualization.Query(dormDataURL);
-  // Update the pie chart once an hour.
+  // Update the chart once an hour.
   dormDataQuery.setRefreshInterval(3600);
-
   // Set a callback to run when the dorm data has been retrieved.
   dormDataQuery.send(displayDormData);
 }
 
 
-// Callback that gets the dorm data and creates DataViews to display each table individually.
+/**
+ * Once data is available, create view to display.
+ */
 function displayDormData(response) {
   // Process errors, if any.
   if (response.isError()) {
@@ -27,6 +30,9 @@ function displayDormData(response) {
   
   // Get the dorm data table.
   var data = response.getDataTable();
+  // Format the energy timestamp column. 
+  var formatter = new google.visualization.DateFormat({formatType: 'short'});
+  formatter.format(data, 5);
   
   // Create the view that is just the total dorm energy data.
   var view = new google.visualization.DataView(data);
@@ -34,11 +40,12 @@ function displayDormData(response) {
   view.setRows([5,11,17]);
   var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
   chart.draw(view, {backgroundColor: '#F5F3E5',
-                    colors: ['#459E00'],
-                    width: 300, 
+                    colors: ['#459E00'],width: 300, 
                     height: 200, 
                     title: 'Energy Usage since 5/25/2010',
                     legend: 'none',
                     hAxis: {minValue: '0', title: 'watt-hours'}
                     });
+  // Print out the timestamp for Ilima-dorm wide energy to give a sense for the last update.
+  document.getElementById('chart_timestamp_div').innerHTML = 'Updated ' + data.getFormattedValue(5, 5);                          
 }
