35f5266219412f07b26e9195becae58e437a64d6
George Lee <keoki.lee@gmail.com>
2010-06-13 14:52:56 -1000

    Renamed kukui_cup_auth to makahiki_auth

--- apps/makahiki_auth/models.py
+++ apps/makahiki_auth/models.py
@@ -1,10 +1,10 @@
 from django_cas.backends import CASBackend
 
-class KukuiCupCASBackend(CASBackend):
+class MakahikiCASBackend(CASBackend):
   """Checks if the login name is a admin or a participant in the cup."""
   
   def authenticate(self, ticket, service):
-    user = super(KukuiCupCASBackend, self).authenticate(ticket, service)
+    user = super(MakahikiCASBackend, self).authenticate(ticket, service)
     
     if len(user.email) == 0:
       # Not a valid user.
