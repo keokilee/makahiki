ebad5774ad92b8a871f16d389deca32d9b29e722
George Lee <keoki.lee@gmail.com>
2010-06-20 22:05:37 -1000

    Adding form for posting to the wall.

--- apps/floors/urls.py
+++ apps/floors/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^(?P<dorm_slug>[-\w]+)/$', 'floors.views.dorm', name='dorm_detail'),
     url(r'^(?P<dorm_slug>[-\w]+)/floor/(?P<floor_slug>[-\w]+)/$', 'floors.views.floor', name='floor_detail'),
+    url(r'^(?P<dorm_slug>[-\w]+)/floor/(?P<floor_slug>[-\w]+)/wall_post/$', 'floors.views.wall_post', name='floor_post'),
 )
--- apps/floors/views.py
+++ apps/floors/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+from django.http import Http404
 
 from floors.models import Dorm, Floor
+from floors.forms import WallForm
 from makahiki_avatar.models import Avatar
 # Create your views here.
 
@@ -14,9 +16,17 @@ def floor(request, dorm_slug, floor_slug):
   
   profiles = floor.profile_set.all()
   posts = floor.post_set.order_by('-created_at')
+  wall_form = WallForm(initial={"floor" : floor.pk})
   
   return render_to_response('floors/floor_detail.html', {
     "profiles": profiles,
     "floor": floor,
     "posts": posts,
+    "wall_form": wall_form,
   }, context_instance = RequestContext(request))
+  
+def wall_post(request, dorm_slug, floor_slug):
+  if request.method == "POST":
+    pass
+  else:
+    raise Http404
