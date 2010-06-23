d8863191b565a9865645eee26dc34702d56f9266
George Lee <keoki.lee@gmail.com>
2010-06-23 12:21:27 -1000

    Updating gauge javascript.  Renamed activities tab to tasks.

--- media/js/gauge.js
+++ media/js/gauge.js
@@ -9,11 +9,10 @@ google.setOnLoadCallback(initializeGauge);
  */
 function initializeGauge() {
   // Get all of the dorm data from the spreadsheet.
-  var gaugeURL = 'http://spreadsheets.google.com/tq?key=0Av0U6TKHfzXYdG1vUnduR0RVTktyR1ZtNjAtSE9Qbmc&range=A3:F21&gid=0';
-  var gaugeQuery = new google.visualization.Query(gaugeURL);
-
-  // Update the gauges every 30 seconds
-  gaugeQuery.setRefreshInterval(30);
+  var gaugeUrl = 'http://spreadsheets.google.com/tq?key=0Av0U6TKHfzXYdG1vUnduR0RVTktyR1ZtNjAtSE9Qbmc&range=A3:F21&gid=0';
+  var gaugeQuery = new google.visualization.Query(gaugeUrl);
+  // Update the gauges every 5 seconds
+  gaugeQuery.setRefreshInterval(5);
 
   // Set a callback to run when the dorm data has been retrieved.
   gaugeQuery.send(displayGaugeData);
@@ -31,27 +30,43 @@ function displayGaugeData(response) {
   }
   
   // Get the dorm data table.
-  var gaugeData = response.getDataTable();
+  var data = response.getDataTable();
   // Format the timestamps in column 3 to a short version. 
-  var gaugeFormatter = new google.visualization.DateFormat({formatType: 'short'});
-  gaugeFormatter.format(gaugeData, 3);
+  var formatter = new google.visualization.DateFormat({formatType: 'short'});
+  formatter.format(data, 3);
+  
+  // Sets up the display to be centered and such that the gauge and text have the same width.
+  var divWidth = 120;
+  setChartDivStyle(divWidth);
   
   // Now draw the gauges.
-  drawGaugeInfo(gaugeData, 17, 'lehua');
-  drawGaugeInfo(gaugeData, 11, 'mokihana');
-  drawGaugeInfo(gaugeData, 5, 'ilima');
+  drawGaugeInfo(data, 17, 'lehua', divWidth);
+  drawGaugeInfo(data, 11, 'mokihana', divWidth);
+  drawGaugeInfo(data, 5, 'ilima', divWidth);
 }
 
 /**
  * Draws the gauge and timestamp for the given dorm. 
  */
-function drawGaugeInfo(data, row, dorm) {
-  var gaugeView = new google.visualization.DataView(data);
-  gaugeView.setColumns([0,2]);
-  gaugeView.setRows([row]);
-  var gauge = new google.visualization.Gauge(document.getElementById(dorm + '_chart_div'));
-  // 90 is the minimum height for a gauge that still allows the dorm name to be readable.
-  gauge.draw(gaugeView, {height: 90, max: 5000, greenFrom: 0, greenTo: 2000, yellowFrom:2001, yellowTo: 4000, redFrom: 4001, redTo: 5000});
+function drawGaugeInfo(data, row, dorm, divWidth) {
+  // First, blank out the title cell, since we'll display it separately.
+  data.setCell(row, 0, '');
+  // Now create the data view.  
+  var chartView = new google.visualization.DataView(data);
+  chartView.setColumns([0,2]);
+  chartView.setRows([row]);
+  var chart = new google.visualization.Gauge(document.getElementById(dorm + '_chart_div'));
+  // 100 is the minimum height for a gauge that still allows the dorm name to be readable.
+  chart.draw(chartView, {height: divWidth, max: 5000, greenFrom: 0, greenTo: 2000, yellowFrom: 2001, yellowTo: 4000, redFrom: 4001, redTo: 5000});
   document.getElementById(dorm + '_time_div').innerHTML = data.getFormattedValue(row, 3);
+  document.getElementById(dorm + '_title_div').innerHTML = dorm;
+  document.getElementById(dorm + '_title_div').style.fontWeight = 'bold';
 }
 
+/**
+ * Sets the style for the outermost div to be the same width as the width of the gauges.
+ */
+function setChartDivStyle(width) {
+  document.getElementById('gaugediv').style.textAlign = "center";
+  document.getElementById('gaugediv').style.width = width;
+}
