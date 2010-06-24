ccda4073bb7b4291129cad0c689f39dc468450ef
George Lee <keoki.lee@gmail.com>
2010-06-24 13:38:30 -1000

    Updating data dump script. Updating settings for new version.

--- dump_data.sh
+++
@@ -1,2 +0,0 @@
-python manage.py dumpdata auth activities makahiki_base floors django_generic_flatblocks gblocks --indent 1 > fixtures/initial_data.json
-
---
+++ scripts/dump_data.sh
@@ -0,0 +1,2 @@
+python manage.py dumpdata auth activities floors makahiki_base floors django_generic_flatblocks gblocks --indent 1 > fixtures/initial_data.json
+
