ab67f2cc99d5e51d51cff768c4e753379e8580f5
George Lee <keoki.lee@gmail.com>
2010-06-30 14:35:18 -1000

    Updating script to add function that adds user to a given floor.

--- scripts/kukuicup.py
+++ scripts/kukuicup.py
@@ -71,6 +71,7 @@ def generate_from_csdl():
     "randycox": {"name": "Randy Cox", "avatar": "randycox.jpeg"},
     "rosemary": {"name": "Rosemary Andrada Sumajit", "avatar": "rosemary.png"},
     "russell": {"name": "Russell Tokuyama", "avatar": "russell.jpeg"},
+    "joedane": {"name": "Joe Dane", "avatar": "joedane.jpeg"}
   }
 
   initials = "abcdefghijklmnopqrstuvwxyz"
@@ -90,6 +91,62 @@ def generate_from_csdl():
     profile = user.get_profile()
     profile.name = members[key]["name"]
     profile.floor = random.sample(floors, 1)[0]
+    profile.save()
+
+    avatar_path = avatar_base + members[key]["avatar"]
+    avatar = Avatar(user=user, primary=True, avatar=avatar_path)
+    avatar.save()
+    
+def generate_csdl_for_floor(floor):
+  from makahiki_avatar.models import Avatar
+  from django.contrib.auth.models import User
+  from floors.models import Floor
+  avatar_base = "avatars/sample/"
+
+  members = {
+    "danport": {"name": "Dan Port", "avatar": "danport.jpg"},
+    "kagawaa": {"name": "Aaron Kagawa", "avatar": "aaronkagawa.jpeg"},
+    "alexey": {"name": "Alexey Olkov", "avatar": "alexey.jpg"},
+    "anned": {"name": "Anne Disney", "avatar": "annedisney.jpeg"},
+    "austen": {"name": "Austen Ito", "avatar": "austen.jpg"},
+    "carleton": {"name": "Carleton Moore", "avatar": "carleton.jpg"},
+    "cedric": {"name": "Cedric Zhang", "avatar": "cedric.jpeg"},
+    "christoph": {"name": "Christoph Lofi", "avatar": "christoph.jpg"},
+    "dadong": {"name": "Dadong Wan", "avatar": "dadong.png"},
+    "danu": {"name": "Danu Tjahjono", "avatar": "danu.png"},
+    "dnickels": {"name": "David Nickels", "avatar": "davidnickels.jpg"},
+    "hongbing": {"name": "Hongbing Kou", "avatar": "hongbing.jpeg"},
+    "jameswang": {"name": "James Wang", "avatar": "jameswang.jpg"},
+    "corbett": {"name": "Jay Corbett", "avatar": "jaycorbett.jpeg"},
+    "jgeis": {"name": "Jennifer Geis", "avatar": "jennifergeis.jpeg"},
+    "jitender": {"name": "Jitender Miglani", "avatar": "jitender.jpeg"},
+    "jaugustin": {"name": "Joy Augustin", "avatar": "joyaugustin.jpeg"},
+    "jsakuda": {"name": "Julie Sakuda", "avatar": "julie.jpg"},
+    "julio": {"name": "Julio Polo", "avatar": "juliopolo.jpeg"},
+    "kyleung": {"name": "Ka Yee Leung", "avatar": "kayeeleung.jpeg"},
+    "mette": {"name": "Mette Moffett", "avatar": "mette.jpeg"},
+    "staver": {"name": "Michael Staver", "avatar": "michaelstaver.jpeg"},
+    "paulding": {"name": "Mike Paulding", "avatar": "mikepaulding.jpg"},
+    "monir": {"name": "Monir Hodges", "avatar": "monir.jpeg"},
+    "pavel": {"name": "Pavel Senin", "avatar": "pavel.jpg"},
+    "randycox": {"name": "Randy Cox", "avatar": "randycox.jpeg"},
+    "rosemary": {"name": "Rosemary Andrada Sumajit", "avatar": "rosemary.png"},
+    "russell": {"name": "Russell Tokuyama", "avatar": "russell.jpeg"},
+    "joedane": {"name": "Joe Dane", "avatar": "joedane.jpeg"}
+  }
+
+  for key in members.keys
+    user = None
+    try:
+      user = User(username=key)
+      user.save()
+    except IntegrityError:
+      print "Integrity Error: Duplicate user."
+      exit()
+
+    profile = user.get_profile()
+    profile.name = members[key]["name"]
+    profile.floor = floor
     profile.save()
 
     avatar_path = avatar_base + members[key]["avatar"]
