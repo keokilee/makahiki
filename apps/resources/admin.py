8f1e1a6fed02907d644ee7b84a52cc2838e31693
George Lee <keoki.lee@gmail.com>
2010-07-05 14:18:43 -1000

    Adding resources models and admin site.  Renaming initial_data.json so that it doesn't blow anything away.

---
+++ apps/resources/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from resources.models import Topic, Resource
+
+admin.site.register(Topic)
+admin.site.register(Resource)
---
+++ apps/resources/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+class Topic(models.Model):
+  topic = models.CharField(max_length=255)
+
+class Resource(models.Model):
+  MEDIA_TYPES = (
+    ('Website', 'Website'),
+    ('Video', 'Video'),
+    ('Audio', 'Audio'),
+    ('Blog Posting', 'Blog Posting'),
+    ('News', 'News'),
+    ('Article', 'Article'),
+  )
+  
+  created_at = models.DateTimeField(
+    default=datetime.datetime.today(),
+    editable=False,
+  )
+  updated_at = models.DateTimeField(
+    default=datetime.datetime.today(),
+    editable=False,
+  )
+  title = models.CharField(
+    max_length=255,
+    help_text="The title of the resource.",
+  )
+  abstract = models.TextField(
+    help_text="A one paragraph, concise summary of the contents. Uses <a href=\"http://daringfireball.net/projects/markdown/\" target=\"_blank\">Markdown</a> formatting.",
+  )
+  topics = models.ManyToManyField(Topic)
+  media_type = models.CharField(
+    max_length=50, 
+    choices=MEDIA_TYPES,
+    help_text="The format of the resource."
+  )
+  length = models.IntegerField(
+    null=True,
+    blank=True,
+    help_text="If appropriate, specify length in the approximate number of pages (for text), minutes duration for video/audio resources.",
+  )
+  url = models.URLField(
+    help_text="Provide the full URL (beginning with http:) to this resource.",
+  )
+  added_by = models.CharField(
+    max_length=255,
+    help_text="Please put your first name here so we know who entered this info in case we have questions later."
+  )
+  
+  def save(self):
+    """Custom save method to update the updated_at field."""
+    self.updated_at = datetime.datetime.today()
+    super(Resource, self).save()
+  
