e82388f7cc0609edc86da5d52d543c231766a41e
George Lee <keoki.lee@gmail.com>
2010-07-12 14:43:21 -1000

    Added view all link, which is non-functional for now.

--- apps/resources/views.py
+++ apps/resources/views.py
@@ -2,22 +2,28 @@ from django.template import RequestContext
 from resources.models import Resource, Topic
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import Http404
+
+from resources import DEFAULT_NUM_RESOURCES
 # Create your views here.
 
 def index(request):
   """Index page for the resources tab."""
   topics = Topic.objects.all()
-  resources = Resource.objects.order_by("-created_at")[0:50]
+  resources = Resource.objects.order_by("-created_at")[0:DEFAULT_NUM_RESOURCES]
+  more_resources = False
+  if Resource.objects.count() > DEFAULT_NUM_RESOURCES:
+    more_resources = True
+    
   return render_to_response('resources/index.html', {
     "topics": topics,
     "resources": resources,
+    "more": more_resources,
   }, context_instance = RequestContext(request))
   
 def topic(request, topic_id):
   """View resources for a given topic."""
   topic = get_object_or_404(Topic, pk=topic_id)
   resources = topic.resource_set.all()
-  
   return render_to_response('resources/list.html', {
     "topic": topic.topic,
     "resources": resources,
