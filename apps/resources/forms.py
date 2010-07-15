ddcd8b331cd6d0a29f28f581ca2e41a10141af34
George Lee <keoki.lee@gmail.com>
2010-07-15 11:37:38 -1000

    Changed table background on hover. Made table rows clickable instead of having titles as links.

--- apps/resources/forms.py
+++ apps/resources/forms.py
@@ -4,13 +4,18 @@ from django.forms.util import ErrorList
 from resources.models import Topic, Resource
   
 class TopicChoiceField(forms.ModelMultipleChoiceField):
+  """Custom choice field to customize the label for each topic."""
   def label_from_instance(self, obj):
     return "%s (%d)" % (obj.topic, obj.resource_set.count())
 
 class TopicSelectForm(forms.Form):
+  """Form for selecting topics in the resource list."""
   topics = TopicChoiceField(
       label="",
       queryset=Topic.objects,
       widget=forms.CheckboxSelectMultiple,
       initial=[topic.pk for topic in Topic.objects.all()],
   )
+  
+# class ViewAllForm(forms.Form):
+  
