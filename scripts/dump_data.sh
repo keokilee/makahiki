88de89020c06674d1cda653045e18e1c77431fca
George Lee <keoki.lee@gmail.com>
2010-07-22 13:01:15 -1000

    Dumping base_data and user_data from dasha to use as fixtures.

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -1,3 +1,3 @@
-python manage.py dumpdata activities floors makahiki_base floors django_generic_flatblocks gblocks --indent 1 > fixtures/base_data.json
+python manage.py dumpdata activities makahiki_base django_generic_flatblocks gblocks --indent 1 > fixtures/base_data.json
 
 
--- scripts/dump_users.sh
+++ scripts/dump_users.sh
@@ -1,2 +1,2 @@
-python manage.py dumpdata auth makahiki_profiles makahiki_avatar --indent 1 > fixtures/user_data.json
+python manage.py dumpdata auth makahiki_profiles makahiki_avatar floors --indent 1 > fixtures/user_data.json
 
