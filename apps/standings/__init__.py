e472b8d63af5be1ac938a53962908cdf0255f9fa
George Lee <keoki.lee@gmail.com>
2010-07-24 17:19:22 -1000

    Completed standings for user in a floor with tests.

--- apps/standings/__init__.py
+++ apps/standings/__init__.py
@@ -22,15 +22,23 @@ def get_standings_for_user(user, standings_group="floor"):
     return ""
   
   if standings_group == "floor":
-    profiles = Profile.objects.filter(floor=user_profile.floor).order_by("-points")
+    profiles = Profile.objects.filter(floor=user_profile.floor).order_by("-points", "-last_awarded_submission")
     title = "Individual standings, %s" % user_profile.floor
     
   elif standings_group == "all":
-    profiles = Profile.objects.all().order_by("-points")
+    profiles = Profile.objects.all().order_by("-points", "-last_awarded_submission")
   else:
     raise StandingsException("Unknown standings type %s" % standings_type)
     
-  info, user_index = _calculate_standings(user_profile, profiles)
+  info, user_index = _calculate_user_standings(user_profile, profiles)
+  
+  # Construct return dictionary.
+  return json.dumps({
+    "title": title,
+    "info": info,
+    "myindex": user_index,
+    "type": standings_type,
+  })
   
 def _calculate_user_standings(user_profile, profiles):
   """Finds user standings based on the user's profile and a list of profiles.
@@ -38,31 +46,49 @@ def _calculate_user_standings(user_profile, profiles):
   
   # First and last users are easy enough to retrieve.
   first = profiles[0]
-  last = profiles[len(profiles)-1]
+  profile_count = profiles.count()
+  last = profiles[profile_count-1]
   
   # Search for user.
   rank = 1
   above_points = first.points
   below_points = last.points
+  found_user = False
   for profile in profiles:
-    # Going to group together users who are tied for now.
-    if profile.points > user_profile.points:
+    if profile == user_profile:
+      # Set flag that we found the user.
+      found_user = True
+    elif not found_user:
+      # If we haven't found the user yet, keep going.
       above_points = profile.points
       rank += 1
-    elif profile.points < user_profile.points:
+    elif found_user:
+      # If we found the user, then this is the person just after.
       below_points = profile.points
       break
       
   # Construct the return dictionary.
   index = 0
   info = [{"points": first.points, "rank": 1}]
-  if rank != 1:
+  
+  # Append above points
+  if rank == 2:
+    # There's no above points, since that is #1
+    index = 1
+  elif rank > 2:
     info.append({"points": above_points, "rank": rank - 1})
-    info.append({"points": user_profile.points, "rank": rank})
     index = 2
-  if user_profile.points > below_points:
-    info.append({"points": below_points, "rank": rank + 1})
-    info.append({"points": last.points, "rank": len(profiles)})
+  
+  # Append user points if they are not #1
+  if rank > 1:
+    info.append({"points": user_profile.points, "rank": rank})
+
+  # Append below and/or last only if the user is not ranked last.
+  if rank < profile_count:
+    if rank < profile_count - 1:
+      # Append the below points if the user is ranked higher than second to last.
+      info.append({"points": below_points, "rank": rank + 1})
+    info.append({"points": last.points, "rank": profile_count})
     
   return info, index
     
--- apps/standings/tests.py
+++ apps/standings/tests.py
@@ -1,14 +1,109 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
+import simplejson as json
+import datetime
 
 from django.test import TestCase
-from standings import get_standings_for_user, StandingsException
-from django.contrib.auth.models import User
+from floors.models import Floor
+from standings import get_standings_for_user
     
-class UserStandingsTest(TestCase):
+class FloorStandingsTest(TestCase):
   fixtures = ["base_data.json", "user_data.json"]
   
+  def setUp(self):
+    self.floor = Floor.objects.all()[0]
+    self.profiles = self.floor.profile_set.order_by("-points", "-last_awarded_submission")
+    self.count = self.profiles.count()
+    
+  def testStandingsFirstPlace(self):
+    """Test that the standings of the first place user is correct."""
+    
+    json_standings = get_standings_for_user(self.profiles[0], "floor")
+    decoded_standings = json.loads(json_standings)
+    
+    # Verify that the returned structure is correct.
+    self.assertTrue(decoded_standings.has_key("title"))
+    self.assertTrue(decoded_standings.has_key("info"))
+    self.assertTrue(decoded_standings.has_key("myindex"))
+    self.assertTrue(decoded_standings.has_key("type"))
+    self.assertTrue(len(decoded_standings) == 4)
+    
+    # Verify that the contents are correct.
+    self.assertTrue(decoded_standings["myindex"] == 0)
+    self.assertTrue(len(decoded_standings["info"]) == 3)
+    self.assertTrue(decoded_standings["info"][0]["rank"] == 1)
+    self.assertTrue(decoded_standings["info"][2]["rank"] == self.count)
+    
+    first = decoded_standings["info"][0]
+    second = decoded_standings["info"][1]
+    self.assertTrue(first["points"] >= second["points"])
+    
+  def testStandingsSecondPlace(self):
+    """Test that standings of the second place user is correct."""
+
+    json_standings = get_standings_for_user(self.profiles[1], "floor")
+    decoded_standings = json.loads(json_standings)
+    self.assertTrue(decoded_standings["myindex"] == 1)
+    self.assertTrue(len(decoded_standings["info"]) == 4)
+    self.assertTrue(decoded_standings["info"][1]["rank"] == 2)
+    
+  def testStandingsThirdPlace(self):
+    """Test that standings of the third place user is correct. This will also verify that any user in the middle is correct."""
+
+    json_standings = get_standings_for_user(self.profiles[2], "floor")
+    decoded_standings = json.loads(json_standings)
+    self.assertTrue(decoded_standings["myindex"] == 2)
+    self.assertTrue(len(decoded_standings["info"]) == 5)
+    self.assertTrue(decoded_standings["info"][2]["rank"] == 3)
+    
+  def testStandingsSecondToLastPlace(self):
+    """Test that standings of the second to the last user is correct."""
+
+    json_standings = get_standings_for_user(self.profiles[self.count-2], "floor")
+    decoded_standings = json.loads(json_standings)
+    self.assertTrue(decoded_standings["myindex"] == 2)
+    self.assertTrue(len(decoded_standings["info"]) == 4)
+      
+  def testStandingsLastPlace(self):
+    """Test that standings of the last place user is correct."""
+
+    json_standings = get_standings_for_user(self.profiles[self.count-1], "floor")
+    decoded_standings = json.loads(json_standings)
+    self.assertTrue(decoded_standings["myindex"] == 2)
+    self.assertTrue(len(decoded_standings["info"]) == 3)
+    self.assertTrue(decoded_standings["info"][2]["rank"] == self.count)
+    
+  def testAddPointsChangeStandings(self):
+    """Test that adding points moves the user up."""
+
+    # Test using the second place user.
+    profile = self.profiles[1]
+    json_standings = get_standings_for_user(profile, "floor")
+    decoded_standings = json.loads(json_standings)
+    point_diff = decoded_standings["info"][0]["points"] - decoded_standings["info"][1]["points"]
+    profile.points += point_diff + 1
+    profile.save()
+
+    json_standings = get_standings_for_user(profile, "floor")
+    decoded_standings = json.loads(json_standings)
+
+    # Verify that user is now first.
+    self.assertTrue(decoded_standings["myindex"] == 0)
+    self.assertTrue(len(decoded_standings["info"]) == 3)
+    
+  def testChangeSubmitDateChangeStandings(self):
+    """Test that tied users with different submission dates are ordered correctly."""
+
+    # Test using the second place user.
+    profile = self.profiles[1]
+    json_standings = get_standings_for_user(profile, "floor")
+    decoded_standings = json.loads(json_standings)
+    point_diff = decoded_standings["info"][0]["points"] - decoded_standings["info"][1]["points"]
+    profile.points += point_diff # Tie for points
+    profile.last_awarded_submission = datetime.datetime.today()
+    profile.save()
+
+    json_standings = get_standings_for_user(profile, "floor")
+    decoded_standings = json.loads(json_standings)
+
+    # Verify that user is now first.
+    self.assertTrue(decoded_standings["myindex"] == 0)
+    self.assertTrue(len(decoded_standings["info"]) == 3)
