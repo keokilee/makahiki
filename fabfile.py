0900133a58690086b280eeb39b76cba991ec0ca7
George Lee <keoki.lee@gmail.com>
2010-08-05 12:21:13 -1000

    Adding initial fabfile.

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+unit_tests = ["activities.tests", "makahiki_base", "standings", "makahiki_profiles.tests"]
+selenium_tests = ["activities"]
+
+def run_all_tests_with_report():
+  """Runs all of the tests and generates a report."""
+  command = "python manage.py test " + string.join(unit_tests + selenium_tests, " ") + " --with-selenium --with-xunit"
+  result = local(command, capture=False)
+  
+def run_tests():
+  """Runs all of the tests and does not append a report."""
+  command = "python manage.py test " + string.join(unit_tests + selenium_tests, " ") + " --with-selenium"
+  result = local(command, capture=False)
+  
+def push_master():
+  local("git checkout master")
+  run_tests()
+  local("git push origin master")
