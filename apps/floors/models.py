f980dfe68d0396569d7ddaca3450b98a4c2b35a6
George Lee <keoki.lee@gmail.com>
2010-08-11 17:10:15 -1000

    Changing slug to a slug field so that it can be indexed.

--- apps/floors/models.py
+++ apps/floors/models.py
@@ -5,43 +5,28 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.template.defaultfilters import slugify
 
 from groups.base import Group
 
 # Create your models here.
 
 class Dorm(models.Model):
+  # Automatically populate slug field when the name is added.
+  prepopulated_fields = {"slug": ("name",)}
+  
   name = models.CharField(max_length=200, help_text="The name of the dorm.")
-  slug = models.CharField(max_length=20, blank=True, help_text="Automatically generated if left blank.")
+  slug = models.SlugField(max_length=20, help_text="Automatically generated if left blank.")
   created_at = models.DateTimeField(editable=False);
   updated_at = models.DateTimeField(null=True, editable=False)
   
   def __unicode__(self):
     return self.name
-  
-  def create_slug(self):
-    """Creates a slug (a url parameter based on content of the title).
-    Returns None if the floor has no title."""
     
-    if not self.name:
-      return None
-      
-    if self.slug:
-      return self.slug
-    
-    slug = self.name
-    for char in string.punctuation:
-      slug = slug.replace(char, "")
-    slug = string.join(slug.split(), "-").lower()
-    if len(slug) > 20:
-      slug = slug[:20]
-    
-    return slug
-    
-  def save(self):
+  def save(self, *args, **kwargs):
     """Custom save method to generate slug and set created_at/updated_at."""
     if not self.slug:
-      self.slug = self.create_slug()
+      self.slug = slugify(self.name)
     
     if not self.created_at:
       self.created_at = datetime.date.today()
@@ -51,8 +36,10 @@ class Dorm(models.Model):
     super(Dorm, self).save()
     
 class Floor(models.Model):
+  prepopulated_fields = {"slug": ("number",)}
+  
   number = models.CharField(help_text="The floor number in the dorm. Can be a string value", max_length=10)
-  slug = models.CharField(max_length=10, blank=True, help_text="Automatically generated if left blank.")
+  slug = models.SlugField(max_length=10, help_text="Automatically generated if left blank.")
   dorm = models.ForeignKey(Dorm, help_text="The dorm this floor belongs to.")
   floor_identifier = models.CharField(
                   max_length=200,
@@ -62,37 +49,14 @@ class Floor(models.Model):
   )
   
   def __unicode__(self):
-    return "%s: Floor %s" % (self.dorm.name, self.slug)
-    
-  def create_slug(self):
-    """Creates a slug (a url parameter based on content of the title).
-    Returns None if the floor has no title."""
-
-    if not self.number:
-      return None
-
-    if self.slug:
-      return self.slug
-
-    slug = self.number
-    for char in string.punctuation:
-      slug = slug.replace(char, "")
-    slug = string.join(slug.split(), "-").lower()
-    if len(slug) > 10:
-      slug = slug[:10]
-
-    return slug
+    return "%s: Floor %s" % (self.dorm.name, self.number)
     
   def save(self):
     """Custom save method to generate slug and set created_at/updated_at."""
     if not self.slug:
-      self.slug = self.create_slug()
+      self.slug = slugify(self.number)
 
     super(Floor, self).save()
-    
-  def get_wattdepot_host(self):
-    """Retrieves the floor's specified host or the host specified in settings."""
-    return self.wattdepot_host or settings.WATTDEPOT_HOST
     
 class Post(models.Model):
   """Represents a wall post on a user's wall."""
