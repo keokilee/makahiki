73e0553fcc91c08def5847c2c385fea55555121d
George Lee <keoki.lee@gmail.com>
2010-08-13 14:49:33 -1000

    Changing slug field in articles to use the built in SlugField, which will automagically generate indexes.

--- apps/makahiki_base/models.py
+++ apps/makahiki_base/models.py
@@ -5,13 +5,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from django.template.defaultfilters import slugify
 
 from floors.models import Floor
 
 # Create your models here.
 
 class Like(models.Model):
-  """Tracks the activities that users like."""
+  """Tracks the objects that users like."""
   user = models.ForeignKey(User)
   floor = models.ForeignKey(Floor)
   content_type = models.ForeignKey(ContentType)
@@ -20,9 +21,10 @@ class Like(models.Model):
 
 class Article(models.Model):
   """Represents an article on the front page."""
+  prepopulated_fields = {"slug": ("title",)}
   
   title = models.CharField(max_length=255)
-  slug = models.CharField(max_length=50, blank=True, help_text="Automatically generated if left blank.")
+  slug = models.SlugField(max_length=50, blank=True, help_text="Automatically generated if left blank.")
   abstract = models.TextField(help_text="Short description of the story.  Uses Markdown formatting.")
   content = models.TextField(help_text="Uses Markdown formatting.")
   created_at = models.DateTimeField(editable=False)
@@ -30,45 +32,17 @@ class Article(models.Model):
   
   def __unicode__(self):
     return self.title
-  
-  def create_slug(self):
-    """Creates a slug (an url parameter based on content of the article) from the article's title.
-    Returns None if the article has no title."""
-    
-    if not self.title:
-      return None
-      
-    if self.slug:
-      return self.slug
-    
-    slug = self.title
-    for char in string.punctuation:
-      slug = slug.replace(char, "")
-    slug = string.join(slug.split(), "-").lower()
-    if len(slug) > 50:
-      slug = slug[:50]
-    
-    return slug
     
   def formatted_date(self):
     """Formats the created or updated date into a pretty string."""
     date = self.updated_at or self.created_at
     return date.strftime("%m/%d %I:%M %p")
     
-  def create_headline(self):
-    """Creates a headline for new stories."""
-    
-    # We want to do this after saving, but not for updated items.
-    if self.pk and not self.updated_at:
-      content = "Latest News: <a href='/news/%d/%s'>%s</a>" % (self.pk, self.slug, self.title)
-      headline = Headline(content=content)
-      headline.save()
-    
   def save(self):
     """Custom save method to update slug and date time fields and to post headlines."""
     
     if not self.slug:
-      self.slug = self.create_slug()
+      self.slug = slugify(self.title)
     
     if not self.id:
       self.created_at = datetime.datetime.today()
@@ -77,5 +51,3 @@ class Article(models.Model):
 
     super(Article, self).save()
     
-    # Create headline.
-    self.create_headline()
