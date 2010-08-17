2560113c6394ab5a41bdc4dc20e720262c07154c
George Lee <keoki.lee@gmail.com>
2010-08-17 11:13:20 -1000

    Adding test to verify JSON retrieval.

--- apps/api/tests.py
+++ apps/api/tests.py
@@ -1,23 +1,51 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
+import simplejson as json
+import datetime
 
 from django.test import TestCase
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class RoundApiFunctionalTest(TestCase):
+  """
+  Tests the API through the Django test client.
+  """
+  
+  def setUp(self):
+    """Set the competition settings to the current date for testing."""
+    self.saved_rounds = settings.COMPETITION_ROUNDS
+    self.saved_start = settings.COMPETITION_START
+    self.saved_end = settings.COMPETITION_END
+    start = datetime.date.today()
+    end = start + datetime.timedelta(days=7)
+    
+    settings.COMPETITION_ROUNDS = {
+      "Round 1" : {
+        "start": start.strftime("%Y-%m-%d"),
+        "end": end.strftime("%Y-%m-%d"),
+      },
+    }
+    
+    settings.COMPETITION_START = start.strftime("%Y-%m-%d")
+    settings.COMPETITION_END = end.strftime("%Y-%m-%d")
+    
+  def testJsonResponse(self):
+    """Test that we get the correct JSON API response."""
+    
+    response = self.client.get(reverse('api_rounds'))
+    self.failUnlessEqual(response.status_code, 200)
+    self.assertEqual(response["Content-Type"], "application/json")
+    
+    # Check that the content is correct.
+    json_content = json.loads(response.content)
+    
+    self.assertTrue(json_content.has_key("start"), "Check that the competition start date is included.")
+    self.assertTrue(json_content.has_key("end"), "Check that the competition end date is included.")
+    self.assertTrue(json_content.has_key("Round 1"), "Check that Round 1 is included.")
+    self.assertEqual(len(json_content), 3, "Check that these are the only 3 items.")
+    
+  def tearDown(self):
+    settings.COMPETITION_ROUNDS = self.saved_rounds
+    settings.COMPETITION_START = self.saved_start
+    settings.COMPETITION_END = self.saved_end
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
 
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,13 +1,13 @@
 import competition_settings
 import simplejson as json
 
+from django.http import HttpResponse
+
 def rounds(self):
   """Returns a dictionary containing competition dates."""
   
   rounds = competition_settings.COMPETITION_ROUNDS
   rounds.update({"start": competition_settings.COMPETITION_START, "end": competition_settings.COMPETITION_END})
   
-  return HttpResponse(json.dumps({
-      "info": rounds,
-  }), mimetype='application/json')
+  return HttpResponse(json.dumps(rounds), mimetype='application/json')
   
