4203523c8c16505a7ca3fb844fc32fd9d5cff018
George Lee <keoki.lee@gmail.com>
2010-08-30 12:57:30 -1000

    Minor tweak in settings.  Changed color of subnavigation bar.

--- competition_settings.py
+++ competition_settings.py
@@ -8,7 +8,7 @@ COMPETITION_END = '2010-12-31'
 # The rounds of the competition. Specify dates using "yyyy-mm-dd".
 # Start means the competition will start at midnight on that date.
 # End means the competition will end at midnight of that date.
-# This means that a round that ends on "2010-08-02" will end at midnight of August 2nd.
+# This means that a round that ends on "2010-08-02" will end at 11:59pm of August 2nd.
 COMPETITION_ROUNDS = {
   "Round 1" : {
     "start": '2010-08-09',
