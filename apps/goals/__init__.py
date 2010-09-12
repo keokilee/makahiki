451e23150b5a6aea44449c67e14a75da25c0b4c0
George Lee <keoki.lee@gmail.com>
2010-09-11 16:35:59 -1000

    Adding url to profile box.

--- apps/goals/__init__.py
+++ apps/goals/__init__.py
@@ -5,14 +5,50 @@ def get_info_for_user(user):
   current_goal = EnergyGoal.get_current_goal()
   if current_goal:
     form = None 
+    results_url = None
     in_voting = current_goal.in_voting_period()
     can_vote = in_voting and current_goal.user_can_vote(user)
     if can_vote:
       form = EnergyGoalVotingForm(
           instance=EnergyGoalVote(user=user, goal=current_goal, percent_reduction=current_goal.default_goal)
       )
+    elif in_voting:
+      profile = user.get_profile()
+      results = current_goal.get_floor_results(profile.floor)
+      results_url = generate_chart_url(results)
         
     return {
           "goal": current_goal,
+          "results_url": results_url,
           "form": form,
     }
+    
+def generate_chart_url(results):
+  """Helper method to generate a chart url given the goal's voting results."""
+  # Create base url.
+  base_url = "http://chart.apis.google.com/chart?cht=bhs&chs=150x100&chxt=x,y&chtt=Voting%20Results"
+
+  # Construct the data and label parameters
+  data = "&chd=t:"
+  label = "&chxl=1:"
+  max_votes = 0
+  for result in results:
+    if result["votes"] > max_votes:
+      max_votes = result["votes"]
+
+    label += "|%d%%" % result["percent_reduction"]
+    data += "%d," % result["votes"]
+
+  # Remove last comma from the data parameter.
+  data = data[0:len(data)-1]
+
+  # Add range parameter.
+  data_range = "&chxr=0,0,%d&chds=0,%d" % (max_votes, max_votes)
+
+  # Add background color parameter.
+  bg_color = "&chf=bg,s,F5F3E5"
+
+  # Add data color.
+  data_color = "&chco=459E00"
+
+  return base_url + data + label + data_range + bg_color + data_color
--- apps/goals/views.py
+++ apps/goals/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 
-from makahiki_base import restricted
+from goals import generate_chart_url
 from goals.models import EnergyGoal, EnergyGoalVote
 from goals.forms import EnergyGoalVotingForm
 
@@ -36,41 +36,11 @@ def voting_results(request, goal_id):
   
   profile = request.user.get_profile()
   results = goal.get_floor_results(profile.floor)
-  url = _generate_chart_url(results)
+  url = generate_chart_url(results)
   
   return HttpResponse(json.dumps({
       "results": list(results), # Needed to convert results from a queryset.
       "url": url,
   }), mimetype='application/json')
-  
-def _generate_chart_url(results):
-  """Helper method to generate a chart url given the goal's voting results."""
-  # Create base url.
-  base_url = "http://chart.apis.google.com/chart?cht=bhs&chs=150x100&chxt=x,y&chtt=Voting%20Results"
-  
-  # Construct the data and label parameters
-  data = "&chd=t:"
-  label = "&chxl=1:"
-  max_votes = 0
-  for result in results:
-    if result["votes"] > max_votes:
-      max_votes = result["votes"]
-      
-    label += "|%d%%" % result["percent_reduction"]
-    data += "%d," % result["votes"]
-    
-  # Remove last comma from the data parameter.
-  data = data[0:len(data)-1]
-  
-  # Add range parameter.
-  data_range = "&chxr=0,0,%d&chds=0,%d" % (max_votes, max_votes)
-  
-  # Add background color parameter.
-  bg_color = "&chf=bg,s,F5F3E5"
-  
-  # Add data color.
-  data_color = "&chco=459E00"
-  
-  return base_url + data + label + data_range + bg_color + data_color
-  
+
   
