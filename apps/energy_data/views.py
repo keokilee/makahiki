03bac92c5213ad18af4e7778899a5b270ffb687d
George Lee <keoki.lee@gmail.com>
2010-09-15 14:55:35 -1000

    Created skeleton view.

--- apps/energy_data/views.py
+++ apps/energy_data/views.py
@@ -1,5 +1,9 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 # Create your views here.
 
 def index(request):
   """Provides a list of dorms and floors."""
-  pass
+  return render_to_response("energy_data/index.html", {
+    
+  }, context_instance = RequestContext(request))
