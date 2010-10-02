06b8f0aeeedd319d0e93e60039917378ce12a02a
George Lee <keoki.lee@gmail.com>
2010-10-01 15:05:03 -1000

    Adding missing files.  Added Cupertino theme.

--- apps/floors/forms.py
+++ apps/floors/forms.py
@@ -1,4 +1,4 @@
 from django import forms
   
 class WallForm(forms.Form):
-  post = forms.CharField(widget=forms.Textarea(attrs={'cols': '60', 'rows': '2'}))
+  post = forms.CharField(widget=forms.Textarea(attrs={'cols': '53', 'rows': '2'}))
