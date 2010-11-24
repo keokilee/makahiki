c8d490de82d4c55abb165e133aa7a7d6872e400f
George Lee <keoki.lee@gmail.com>
2010-11-24 13:01:09 -1000

    Committing current status of the setup.

--- apps/setup_wizard/forms.py
+++ apps/setup_wizard/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from django.conf import settings
 
 class TermsForm(forms.Form):
   # Seems that required means that the value must be True.
   accept = forms.BooleanField(widget=forms.HiddenInput(), required=False)
+  
+class FacebookForm(forms.Form):
+  can_post = forms.BooleanField(
+        required=False, 
+        label="%s can post to my Facebook feed (at most 2 posts per day)" % settings.COMPETITION_NAME
+  )
 
 class ProfileForm(forms.Form):
   display_name = forms.CharField(max_length=12)
--- apps/setup_wizard/views.py
+++ apps/setup_wizard/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from makahiki_facebook.models import FacebookProfile
 
 import makahiki_facebook.facebook as fb
-from setup_wizard.forms import TermsForm, ProfileForm
+from setup_wizard.forms import TermsForm, ProfileForm, FacebookForm
 
 def terms(request):
   """Display the terms and conditions."""
@@ -29,14 +29,29 @@ def terms(request):
   
 def facebook(request):
   """Ask the user if they'd like to use Facebook Connect."""
-  return render_to_response("setup_wizard/facebook.html", {}, context_instance=RequestContext(request))
+  
+  if request.method == "POST":
+    form = FacebookForm(request.POST)
+    if form.is_valid():
+      fb_user = fb.get_user_from_cookie(request.COOKIES, settings.FACEBOOK_APP_ID, settings.FACEBOOK_SECRET_KEY)
+      if fb_user:
+        fb_profile = FacebookProfile.create_or_update_from_fb_user(request.user, fb_user)
+        fb_profile.can_post = form.cleaned_data["can_post"]
+        fb_profile.save()
+        
+        return HttpResponseRedirect(reverse("setup_profile"))
+        
+  form = FacebookForm()
+  return render_to_response("setup_wizard/facebook.html", {
+    "form": form,
+  }, context_instance=RequestContext(request))
   
 def profile(request):
   """Set up the user profile and use information from Facebook if available."""
   # We check here if the user logged in using Facebook
   fb_user = fb.get_user_from_cookie(request.COOKIES, settings.FACEBOOK_APP_ID, settings.FACEBOOK_SECRET_KEY)
   if fb_user:
-    fb_profile = FacebookProfile.create_or_update_from_fb_user(request.user, fb_user)
+    fb_profile = request.user.facebookprofile
     form = ProfileForm(initial={"display_name": fb_profile.profile_id, "about": fb_profile.about})
   else:
     form = ProfileForm()
