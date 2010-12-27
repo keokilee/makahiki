c01efbce4539df1e3a5bb38214e4fa8ff15bc004
George Lee <keoki.lee@gmail.com>
2010-12-26 20:02:50 -1000

    Updating help views.

--- apps/pages/help/views.py
+++ apps/pages/help/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def index(request):
+  return render_to_response("index.html", {}, context_instance=RequestContext(request))
