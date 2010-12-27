98a52a9666e9f637706806060d3ca793b177d8d4
George Lee <keoki.lee@gmail.com>
2010-12-26 16:23:53 -1000

    Moved files around and created additional settings file for Makahiki specific settings.

--- competition_settings.py
+++ competition_settings.py
@@ -1,12 +1,3 @@
-# The actual name of the competition.
-COMPETITION_NAME = "Kukui Cup"
-
-# Optional setting to specify a special name for the points. Default is "points".
-COMPETITION_POINT_NAME = "Kukui Nut"
-
-# The name of a standard competition grouping.  Defaults to "Floor" if this is not provided.
-COMPETITION_GROUP_NAME = "Lounge"
-
 # The start and end date of the competition.
 COMPETITION_START = '2010-07-28'
 COMPETITION_END = '2010-12-31'
@@ -19,38 +10,5 @@ COMPETITION_ROUNDS = {
   "Round 1" : {
     "start": '2010-08-09',
     "end": '2010-12-31',
-  },
-}
-
-# The theme to use as default. This corresponds to a folder in media that contains the CSS.
-MAKAHIKI_THEME = "default"
-
-# Theme settings for Makahiki.  Used to aid in styling the Javascript widgets.
-# The system will insert the colors based on the selected theme.  If an entry 
-# for a theme does not exist, default will be used.
-MAKAHIKI_THEME_SETTINGS = {
-  "default" : {
-    "widgetBackgroundColor" : 'F5F3E5',
-    "widgetHeaderColor" : '459E00',
-    "widgetHeaderTextColor" : 'white',
-    "widgetTextColor" : '312E25',
-    "widgetTextFont" : 'Ariel, sans serif',
-    "windowNavBarColor" : '2F6B00',
-  },
-  "cupertino" : {
-    "widgetBackgroundColor" : 'F2F5F7',
-    "widgetHeaderColor" : '459E00',
-    "widgetHeaderTextColor" : 'white',
-    "widgetTextColor" : '312E25',
-    "widgetTextFont" : 'Ariel, sans serif',
-    "windowNavBarColor" : '2F6B00',
-  },
-  "start" : {
-    "widgetBackgroundColor" : 'fff',
-    "widgetHeaderColor" : '459E00',
-    "widgetHeaderTextColor" : 'white',
-    "widgetTextColor" : '312E25',
-    "widgetTextFont" : 'Ariel, sans serif',
-    "windowNavBarColor" : '2F6B00',
   },
 }
