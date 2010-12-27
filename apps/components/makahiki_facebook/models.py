c03f80fe6d11959c8816d3a060c775b90aad48bb
George Lee <keoki.lee@gmail.com>
2010-12-26 19:07:01 -1000

    Cleaning up facebook integration.

--- apps/components/makahiki_facebook/models.py
+++ apps/components/makahiki_facebook/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-import makahiki_facebook.facebook as facebook
+import components.makahiki_facebook.facebook as facebook
 
 # Create your models here.
 
