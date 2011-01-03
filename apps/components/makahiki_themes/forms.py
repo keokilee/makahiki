ce7dcd3959896dccad0251e3ea7d00fbd125c386
George Lee <keoki.lee@gmail.com>
2011-01-03 13:22:37 -1000

    Refactored media directory for better support of the CSS selector.

--- apps/components/makahiki_themes/forms.py
+++ apps/components/makahiki_themes/forms.py
@@ -5,8 +5,8 @@ from django.conf import settings
 
 def _get_installed_themes():
   """Get a list of installed themes to be shown in the select widget."""
-  theme_dir = os.path.join(settings.PROJECT_ROOT, "media")
-  return ((item, item) for item in os.listdir(theme_dir) if os.path.isdir(os.path.join(theme_dir, item, "css")))
+  theme_dir = os.path.join(settings.PROJECT_ROOT, "media/css")
+  return ((item, item) for item in os.listdir(theme_dir) if os.path.isdir(os.path.join(theme_dir, item)))
       
 class ThemeSelect(forms.Form):
   """Form for selecting the installed theme."""
