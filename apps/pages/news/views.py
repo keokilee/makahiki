88867dc53d0a881b4a0324947eca36cd0db12c52
George Lee <keoki.lee@gmail.com>
2011-01-11 08:21:05 -1000

    First go at a news feed.

--- apps/pages/news/views.py
+++ apps/pages/news/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from components.floors.models import Post
+from pages.news.forms import WallForm
+
 def index(request):
-  return render_to_response("news/index.html", {}, context_instance=RequestContext(request))
+  floor = request.user.get_profile().floor
+  posts = Post.objects.filter(floor=floor)[:10]
+  return render_to_response("news/index.html", {
+    "posts": posts,
+    "wall_form": WallForm(),
+  }, context_instance=RequestContext(request))
