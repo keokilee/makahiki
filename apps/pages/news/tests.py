96b6292f54be3c0cc94b730ea8e0ed65763f9e8e
George Lee <keoki.lee@gmail.com>
2011-01-11 23:33:13 -1000

    Tweaking the wall posts.  Not sure what to do about the tests for the time being since it requires being logged in.

--- apps/pages/news/tests.py
+++ apps/pages/news/tests.py
@@ -2,7 +2,11 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 class NewsFunctionalTestCase(TestCase):
+  fixtures = ["base_data.json", "user_data.json"]
+  
   def testIndex(self):
     """Check that we can load the index page."""
+    self.client.login(username="user", password="changeme")
+    
     response = self.client.get(reverse("news_index"))
     self.failUnlessEqual(response.status_code, 200)
