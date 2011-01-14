84005ce3a027e387d8e062128ce70dc8f50ffe88
George Lee <keoki.lee@gmail.com>
2011-01-14 11:48:31 -1000

    Added login to some of the pages tests.  Added some missing links to the profile page.

--- apps/pages/view_activities/tests.py
+++ apps/pages/view_activities/tests.py
@@ -4,5 +4,7 @@ from django.core.urlresolvers import reverse
 class ActivitiesFunctionalTestCase(TestCase):
   def testIndex(self):
     """Check that we can load the index page."""
+    self.client.login(username="user", password="changeme")
+    
     response = self.client.get(reverse("activities_index"))
     self.failUnlessEqual(response.status_code, 200)
--- apps/pages/view_prizes/tests.py
+++ apps/pages/view_prizes/tests.py
@@ -4,5 +4,7 @@ from django.core.urlresolvers import reverse
 class PrizesFunctionalTestCase(TestCase):
   def testIndex(self):
     """Check that we can load the index page."""
+    self.client.login(username="user", password="changeme")
+    
     response = self.client.get(reverse("prizes_index"))
     self.failUnlessEqual(response.status_code, 200)
