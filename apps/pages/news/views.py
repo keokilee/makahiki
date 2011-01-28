d64df96fb79765a0be73ad8153f6191532bc94ac
George Lee <keoki.lee@gmail.com>
2011-01-27 21:12:05 -1000

    Fixing ordering issue with the news page.

--- apps/pages/news/views.py
+++ apps/pages/news/views.py
@@ -8,7 +8,7 @@ from pages.news.forms import WallForm
 @login_required
 def index(request):
   floor = request.user.get_profile().floor
-  posts = Post.objects.filter(floor=floor)[:10]
+  posts = Post.objects.filter(floor=floor).order_by("-id")[:10]
   return render_to_response("news/index.html", {
     "posts": posts,
     "wall_form": WallForm(),
