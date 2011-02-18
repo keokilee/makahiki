12dd83316aca6d7348df9df02a35a9fd6c503b58
George Lee <keoki.lee@gmail.com>
2011-02-17 16:52:59 -1000

    Updating test and removing unnecessary parameter.

--- apps/components/floors/__init__.py
+++ apps/components/floors/__init__.py
@@ -1,2 +1,1 @@
-# Number of wall posts to display at a time.
-NUM_POSTS_TO_DISPLAY = 20
+
--- apps/pages/news/tests.py
+++ apps/pages/news/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
-from components.floors.models import Floor
+from components.floors.models import Floor, Post
+from pages.news import DEFAULT_POST_COUNT
 
 class NewsFunctionalTestCase(TestCase):
   fixtures = ["base_floors.json"]
@@ -21,3 +22,36 @@ class NewsFunctionalTestCase(TestCase):
     
     response = self.client.get(reverse("news_index"))
     self.failUnlessEqual(response.status_code, 200)
+    
+  def testAjaxPosts(self):
+    """Test that we can load new posts via AJAX."""
+    user = User.objects.create_user("user", "user@test.com", password="changeme")
+    floor = Floor.objects.all()[0]
+    profile = user.get_profile()
+    profile.floor = floor
+    profile.setup_complete = True
+    profile.profile_setup = True
+    profile.save()
+    
+    self.client.login(username="user", password="changeme")
+    
+    # Generate test posts.
+    for i in range(0, DEFAULT_POST_COUNT + 1):
+      text = "Testing AJAX response %d." % i
+      post = Post(user=user, floor=floor, text=text)
+      post.save()
+    
+    second_post = Post.objects.all().order_by("-pk")[0]
+    response = self.client.get(reverse("news_more_posts"), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    self.failUnlessEqual(response.status_code, 200)
+    self.assertNotContains(response, "Testing AJAX response 0.")
+    for i in range(1, DEFAULT_POST_COUNT + 1):
+      self.assertContains(response, "Testing AJAX response %d" % i)
+    
+    response = self.client.get(reverse("news_more_posts") + ("?last_post=%d" % second_post.id), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    self.assertContains(response, "Testing AJAX response 0.", 1)
+      
+      
+      
+    
+    
