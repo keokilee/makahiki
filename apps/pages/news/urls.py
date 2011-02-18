1a9e9b48b933b809231cdbfbb3c5582b89f64d94
George Lee <keoki.lee@gmail.com>
2011-02-17 15:17:23 -1000

    Implemented AJAX loading of posts.

--- apps/pages/news/urls.py
+++ apps/pages/news/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'pages.news.views.index', name='news_index'),
+    url(r'^more_posts/$', 'pages.news.views.more_posts', name='news_more_posts'),
 )
--- apps/pages/news/views.py
+++ apps/pages/news/views.py
@@ -1,6 +1,10 @@
+import simplejson as json
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
+from django.http import Http404, HttpResponse
 
 from components.floors.models import Post
 from pages.news.forms import WallForm
@@ -21,3 +25,28 @@ def index(request):
     "wall_form": WallForm(),
     "more_posts": more_posts,
   }, context_instance=RequestContext(request))
+  
+@login_required
+def more_posts(request):
+  if request.is_ajax():
+    floor = request.user.get_profile().floor
+    if request.GET.has_key("last_post"):
+      posts = Post.objects.filter(floor=floor, id__lt=int(request.GET["last_post"])).order_by("-id")
+    else:
+      posts = Post.objects.filter(floor=floor).order_by("-id")
+    
+    post_count = posts.count
+    posts = posts[:DEFAULT_POST_COUNT]
+    more_posts = True if post_count > DEFAULT_POST_COUNT else False
+    
+    template = render_to_string("news/news_posts.html", {
+        "posts": posts,
+        "wall_form": WallForm(),
+        "more_posts": more_posts,
+    }, context_instance=RequestContext(request))
+
+    return HttpResponse(json.dumps({
+        "contents": template,
+    }), mimetype='application/json')
+  
+  raise Http404
