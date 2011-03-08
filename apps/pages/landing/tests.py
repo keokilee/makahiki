5f5e6c812e19d8d2f78547a12656e9ee51665617
George Lee <keoki.lee@gmail.com>
2011-03-08 13:04:09 -1000

    Adding redirect to home page from the landing page for logged in users.

--- apps/pages/landing/tests.py
+++ apps/pages/landing/tests.py
@@ -1,8 +1,29 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+from components.floors.models import Floor
 
 class LandingFunctionalTestCase(TestCase):
+  fixtures = ["base_floors.json"]
+  
   def testLanding(self):
     """Check that we can load the landing page."""
     response = self.client.get(reverse("landing"))
     self.failUnlessEqual(response.status_code, 200)
+    
+  def testLoggedInRedirect(self):
+    """Tests that if the user is logged in, we redirect to the home page."""
+    user = User.objects.create_user("user", "user@test.com", password="changeme")
+    floor = Floor.objects.all()[0]
+    profile = user.get_profile()
+    profile.floor = floor
+    profile.setup_complete = True
+    profile.setup_profile = True
+    profile.save()
+    
+    self.client.login(username="user", password="changeme")
+    
+    response = self.client.get(reverse("landing"))
+    self.assertRedirects(response, reverse("home_index"),
+        msg_prefix="Landing page should redirect to home page for logged in users.")
--- apps/pages/landing/views.py
+++ apps/pages/landing/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
 def index(request):
+  if request.user.is_authenticated():
+    return HttpResponseRedirect(reverse("home_index"))
+    
   return render_to_response("landing/index.html", {}, context_instance=RequestContext(request))
