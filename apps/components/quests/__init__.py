67946f0b57b8d393d255ff37fe3f49b12be82b6f
George Lee <keoki.lee@gmail.com>
2011-03-29 11:40:39 -1000

    Set up the quest models and the backend API.

---
+++ apps/components/quests/__init__.py
@@ -0,0 +1,25 @@
+from components.quests.models import Quest, QuestMember
+
+def check_quest_completion(user):
+  """
+  Check if the user has completed any quests.
+  """
+  pass
+
+def is_complete(user, quest):
+  """
+  Determine if the user has completed the quest.
+  """
+  pass
+  
+def populate_quests(user):
+  """
+  Load available quests for the user.
+  """
+  pass
+  
+def is_available(user, quest):
+  """
+  Determine if the quest is available for the user.
+  """
+  pass
---
+++ apps/components/quests/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Quest(models.Model):
+  """
+  Represents a quest in the database.
+  """
+  name = models.CharField(max_length=255, help_text="The name of the quest.")
+  quest_slug = models.SlugField()
+  description = models.TextField(help_text="Outline the steps to completing this quest.")
+  level = models.IntegerField()
+  unlock_conditions = models.TextField(
+      help_text="Conditions a user needs to meet in order to have this quest be available."
+  )
+  completion_conditions = models.TextField(
+      help_text="Conditions a user needs to meet in order to complete the quest."
+  )
+  created_at = models.DateTimeField(auto_now_add=True, editable=False)
+  updated_at = models.DateTimeField(auto_now=True, editable=False)
+  
+class QuestMember(models.Model):
+  """
+  Represents a user's participation in a quest.
+  This should not be in the admin interface, since there shouldn't be a reason to edit instances.
+  """
+  user = models.ForeignKey(User)
+  quest = models.ForeignKey(Quest)
+  awarded = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
