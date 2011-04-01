99084cbdafdaf30e3103b65c6d3627f31bfb8c95
Yongwen <yongwen.xu@gmail.com>
2011-03-31 21:40:51 -1000

    change page title, add landing about page, add grid color

--- urls.py
+++ urls.py
@@ -13,6 +13,7 @@ import os
 urlpatterns = patterns('',
     # Main pages.
     url(r'^$', "pages.landing.views.index", name="landing"),
+    url(r'^about/', "pages.landing.views.about", name="about"),
     url(r'^home/', include('pages.home.urls')),
     url(r'^activities/', include('pages.view_activities.urls')),
     url(r'^energy/', include('pages.view_energy.urls')),
@@ -36,7 +37,8 @@ urlpatterns = patterns('',
     
     # pinax provided
     (r'^account/', include('account.urls')),
-    (r'^admin/(.*)', include(admin.site.urls)),
+    #(r'^admin/(.*)', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
     # (r'^notifications/', include('notification.urls')),
 )
 
