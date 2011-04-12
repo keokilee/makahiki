d9161b2e1ea10500ae8d9883c47a25ef89f22fff
Nate <nathaniel.h.ashe@gmail.com>
2011-04-12 13:38:06 -1000

    Ignore log file RoundONE.log

--- apps/components/logging/middleware.py
+++ apps/components/logging/middleware.py
@@ -6,16 +6,17 @@ from time import strftime # Timestamp
 class LoggingMiddleware(object):
 
 	def process_response(self, request, response):
-		user = request.user
-		path = request.path
+		if (request.user.is_authenticated()):
+			user = request.user
+			path = request.path
 		
-		# Timestamp yyyy-mm-dd Time
-		timestamp = strftime("%Y-%m-%d %H:%M:%S")
+			# Timestamp yyyy-mm-dd Time
+			timestamp = strftime("%Y-%m-%d %H:%M:%S")
 	
-		if request.META.has_key("HTTP_REFERER"):
-			try:
-				if request.GET["ref"] == "nav-button":
-					logging.info(" " + timestamp + " " + user.username + ' "' + "GET " + request.GET["ref"] + '" ' + path + " 200")
-			except Exception:
-				pass
+			if request.META.has_key("HTTP_REFERER"):
+				try:
+					if request.GET["ref"] == "nav-button":
+						logging.info(" " + timestamp + " " + user.username + ' "' + "GET " + request.GET["ref"] + '" ' + path + " 200")
+				except Exception:
+					pass
 		return response
