fa18d831a004bf70cc98dc3d34a8e1b661226b43
Nate <nathaniel.h.ashe@gmail.com>
2011-04-12 12:54:17 -1000

    Ignored all log files.

--- apps/components/logging/middleware.py
+++ apps/components/logging/middleware.py
@@ -1,12 +1,21 @@
 import logging
 import traceback
 import sys
+from time import strftime # Timestamp
 
 class LoggingMiddleware(object):
 
-	def process_request(self, request):
-		logging.debug("hello world!")
-		logging.info("this is some interesting info!")
-		logging.error("this is an error!")
+	def process_response(self, request, response):
+		user = request.user
+		path = request.path
+		
+		#Timestamp yyyy-mm-dd Time
+		timestamp = strftime("%Y-%m-%d %H:%M:%S")
 	
-		return None
+		if request.META.has_key("HTTP_REFERER"):
+			try:
+				if request.GET["ref"] == "nav-button":
+					logging.info(" " + timestamp + " " + user.username + ' "' + "GET " + request.GET["ref"] + '" ' + path + " 200")
+			except Exception:
+				pass
+		return response
