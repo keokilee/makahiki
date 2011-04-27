09fe0de2a33e0e9c7dd0988ba8b004162bf226b2
George Lee <keoki.lee@gmail.com>
2011-04-27 09:42:16 -1000

    Dumping all quest data, including quest members.

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -2,7 +2,7 @@ python manage.py dumpdata --indent=2 floors.dorm floors.floor > fixtures/base_fl
 
 python manage.py dumpdata --indent=2 activities > fixtures/base_activities.json
 
-python manage.py dumpdata --indent=2 quests.Quest > fixtures/base_quests.json
+python manage.py dumpdata --indent=2 quests > fixtures/base_quests.json
 
 python manage.py dumpdata --indent=2 account auth.user makahiki_profiles.profile > fixtures/test_users.json
 
