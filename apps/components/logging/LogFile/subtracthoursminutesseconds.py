a56be62f50e2ff8d0e571c7141b11ebda5dab4c5
Nate <nathaniel.h.ashe@gmail.com>
2011-05-10 10:23:35 -1000

    Deleted logging testing files.

--- apps/components/logging/LogFile/subtracthoursminutesseconds.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-start = "2011-05-03 03:00:00"
-end = "2011-05-04 04:00:00"
-start_dt = dt.datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
-end_dt = dt.datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
-diff = (end_dt - start_dt) 
-diff.seconds/60 
-# test = dt.datetime.strptime(str(diff), '%Y-%m-%d %H:%M:%S')
-print str(diff)
--- apps/components/logging/LogFile/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import csv
-import datetime as dt
-
-test = ["this is a test"]
-testing = test[0].split()
-testing[2] = "an"
-test[0] = testing
-print test
