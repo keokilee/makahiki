a2e26f065d661265f5d0ab26f896e0972388207c
George Lee <keoki.lee@gmail.com>
2011-06-09 14:50:22 -1000

    Fixing help bug with admin.  Added additional check to see if a topic is a sub-topic of itself.

--- apps/components/help_topics/admin.py
+++ apps/components/help_topics/admin.py
@@ -12,9 +12,12 @@ class HelpAdminForm(forms.ModelForm):
     Prevents sub-topics of sub-topics.  It cannot be done with the current template layout.
     """
     parent_topic = self.cleaned_data["parent_topic"]
-    if parent_topic.parent_topic:
+    if parent_topic and parent_topic.parent_topic:
       raise forms.ValidationError("This topic is also a sub-topic. Sub-topics of sub-topics are not allowed.")
-    return data
+    if parent_topic and parent_topic.slug == self.cleaned_data["slug"]:
+      raise forms.ValidationError("Topic cannot be a sub-topic of itself.")
+      
+    return parent_topic
     
 class HelpTopicAdmin(admin.ModelAdmin):
   # Automatically populates the slug field.
