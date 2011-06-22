0fa9192d194c5bff8da1a233ffbc0acac0142484
Nathan Dorman <dorman.nathan@gmail.com>
2011-06-21 15:12:30 -1000

    Modfied mobile urls.py and views.py.

--- apps/pages/mobile/urls.py
+++ apps/pages/mobile/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'pages.mobile.views.index', name='mobile_index'),
+    url(r'^landing/$', 'pages.mobile.views.landing', name='mobile_landing'),
+    url(r'^smartgrid/$', 'pages.mobile.views.smartgrid', name='mobile_smartgrid'),
 )
