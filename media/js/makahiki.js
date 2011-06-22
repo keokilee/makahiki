c6b3e5a9f71af64415b63ff099262c06a995f9ff
George Lee <keoki.lee@gmail.com>
2011-06-21 18:15:58 -1000

    Updated fabfile since it needed modifications to run on our server.  Committing new js library.

---
+++ media/js/makahiki.js
@@ -0,0 +1,30 @@
+// Function to handle AJAX post requests.
+$.ajaxSetup({ 
+     beforeSend: function(xhr, settings) {
+         function getCookie(name) {
+             var cookieValue = null;
+             if (document.cookie && document.cookie != '') {
+                 var cookies = document.cookie.split(';');
+                 for (var i = 0; i < cookies.length; i++) {
+                     var cookie = jQuery.trim(cookies[i]);
+                     // Does this cookie string begin with the name we want?
+                 if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                     cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                     break;
+                 }
+             }
+         }
+         return cookieValue;
+         }
+         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+             // Only send the token to relative URLs i.e. locally.
+             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+         }
+     } 
+});
+
+// Make sure to load jQuery before this.
+var log_js_action = function(type, object, action) {
+  // Send a request to the logging URL.
+  $.get("/log/" + type + "/" + object + "/" + action + "/");
+}
