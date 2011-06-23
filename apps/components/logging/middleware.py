74118c662f9395b0b3c2b03afad2f20cd361007c
George Lee <keoki.lee@gmail.com>
2011-06-22 15:18:38 -1000

    Added client IP address to logs as well as uploaded filenames. Close #204 and close #201

--- apps/components/logging/middleware.py
+++ apps/components/logging/middleware.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import string
 from time import strftime # Timestamp
 
 from django.conf import settings
@@ -17,19 +18,26 @@ class LoggingMiddleware(object):
       path = request.path
       code = response.status_code
       method = request.method
+      ip_addr = request.META["REMOTE_ADDR"] if request.META.has_key("REMOTE_ADDR") else "no-ip"
       # Timestamp yyyy-mm-dd Time
       timestamp = strftime("%Y-%m-%d %H:%M:%S")
     
       if not re.match(media_regexp, path) and path != "/favicon.ico":
-        entry = "%s %s %s %s %d" % (timestamp, user.username, method, path, code)
+        entry = "%s %s %s %s %s %d" % (timestamp, ip_addr, user.username, method, path, code)
         if request.method == "POST":
           # Dump the POST parameters, but we don't need the CSRF token.
           query_dict = request.POST.copy()
           # print query_dict
           if query_dict.has_key(u"csrfmiddlewaretoken"):
             del query_dict[u'csrfmiddlewaretoken']
-          
+            
           entry += " %s" % (query_dict,)
+          
+        if request.FILES:
+          # Append the filenames to the log.
+          filenames = (f.name for f in request.FILES.values())
+          file_str = "<Files: %s>" % string.join(filenames, " ")
+          entry += " %s" % (file_str,)
         
         logger = logging.getLogger("makahiki_logger")
         logger.info(entry)
