177239e82f7b49c32a494d8e70388a538082d899
Nathan Dorman <dorman.nathan@gmail.com>
2011-06-25 10:08:11 -1000

    smart grid update.

--- apps/pages/mobile/urls.py
+++ apps/pages/mobile/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('',
     url(r'^$', 'pages.mobile.views.index', name='mobile_index'),
     url(r'^landing/$', 'pages.mobile.views.landing', name='mobile_landing'),
     url(r'^smartgrid/$', 'pages.mobile.views.smartgrid', name='mobile_smartgrid'),
+    url(r'^smartgrid/task/(\d+)/$', 'pages.mobile.views.task', name='mobile_smartgrid_task'),
     url(r'^events/$', 'pages.mobile.views.events', name='mobile_events'),
     url(r'^events/sevent/$', 'pages.mobile.views.sevent', name='mobile_sevent')
 )
--- apps/pages/mobile/views.py
+++ apps/pages/mobile/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.http import Http404
+
+from components.activities.models import ActivityBase
 
 def index(request):
   return render_to_response("mobile/index.html", {}, context_instance=RequestContext(request))
 
 def smartgrid(request):
-  return render_to_response("mobile/smartgrid/index.html", {}, context_instance=RequestContext(request))
+  activities = ActivityBase.objects.order_by("category__name")
+
+  return render_to_response("mobile/smartgrid/index.html", {
+    "activities": activities,
+  }, context_instance=RequestContext(request))
+
+def task(request):
+  activity = get_object_or_404(ActivityBase, id=activity_id)
+
+  return render_to_response("mobile/smartgrid/task.html", {
+    "activity": activity,
+  }, context_instance=RequestContext(request))
 
 def landing(request):
   return render_to_response("mobile/landing.html", {}, context_instance=RequestContext(request))
