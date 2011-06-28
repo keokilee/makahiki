a762550ebd9a1170d66d2949859b3430a923321d
epy00n <epy00n@epy00n-laptop.(none)>
2011-06-27 19:10:02 -1000

    modified urls.py

--- apps/pages/mobile/urls.py
+++ apps/pages/mobile/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^landing/$', 'pages.mobile.views.landing', name='mobile_landing'),
     url(r'^smartgrid/$', 'pages.mobile.views.smartgrid', name='mobile_smartgrid'),
     url(r'^smartgrid/task/(\d+)/$', 'pages.mobile.views.task', name='mobile_smartgrid_task'), 
-    url(r'^events/(\w*)/$', 'pages.mobile.views.events', name='mobile_events') 
+    url(r'^events/(\w*)/$', 'pages.mobile.views.events', name='mobile_events'), 
     url(r'^smartgrid/task/(\d+)/response/$', 'pages.mobile.views.sgresponse', name='mobile_smartgrid_response'),
     url(r'^events/$', 'pages.mobile.views.events', name='mobile_events') 
 )
