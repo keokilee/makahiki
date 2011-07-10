76b89c3f49811db745bbcb8d83480e04f76ebfb3
George Lee <keoki.lee@gmail.com>
2011-07-09 18:02:11 -1000

    Adjusted quest logging to handle canopy quests. Moved quest.js to media and imported it in the header.

--- media/js/quest.js
+++ media/js/quest.js
@@ -14,7 +14,12 @@ $(document).ready(function() {
 
       //Toggle quest-details if it is hidden.
       if (!$("#quest-details").is(":visible")) {
-        log_js_action("quest", $(this).attr('id'), 'show');
+        if($(this).parent().hasClass('canopy-quest')) {
+          log_js_action("canopy-quest", $(this).attr('id'), 'show');
+        }
+        else {
+          log_js_action("quest", $(this).attr('id'), 'show'); 
+        }
         // console.log($(this).attr('id'));
         $("#quest-details").slideDown("slow", function() {
           //Animation complete
@@ -25,7 +30,12 @@ $(document).ready(function() {
     else {
       //Hide quest details and remove selected.
       $(this).parent().removeClass("selected");
-      log_js_action("quest", $(this).attr('id'), 'hide');
+      if($(this).parent().hasClass('canopy-quest')) {
+        log_js_action("canopy-quest", $(this).attr('id'), 'hide');
+      }
+      else {
+        log_js_action("quest", $(this).attr('id'), 'hide');
+      }
       $("#quest-details").slideUp("slow", function() {
         //Animation complete
       });
@@ -35,7 +45,12 @@ $(document).ready(function() {
   // Handler for clicking on the hide link.
   $("#quest-hide").click(function() {
     $("#quest-box li").removeClass("selected");
-    log_js_action("quest", $(this).attr('id'), 'hide');
+    if($(this).parent().hasClass('canopy-quest')) {
+      log_js_action("canopy-quest", $(this).attr('id'), 'hide');
+    }
+    else {
+      log_js_action("quest", $(this).attr('id'), 'hide');
+    }
     $("#quest-details").slideUp("slow", function() {
       //Animation complete
     });
