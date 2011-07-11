0061441e271a0528852f9f549b0fc0cc8e92502c
George Lee <keoki.lee@gmail.com>
2011-07-11 12:00:02 -1000

    Added server-side logging of displayed alerts. Close #242

--- apps/components/makahiki_notifications/templatetags/notice_tags.py
+++ apps/components/makahiki_notifications/templatetags/notice_tags.py
@@ -3,12 +3,17 @@ import sys
 from django import template
 from django.template import Library
 
+from components.logging import create_server_log
+
 register = Library()
   
 @register.simple_tag
-def mark_alerts_displayed(alerts):
+def mark_alerts_displayed(request, alerts):
   """
   Simple tag to mark the alerts displayed.
   """
+  for alert in alerts:
+    create_server_log(request, "/slog/notifications/alert/%d/" % alert.pk)
+    
   alerts.update(display_alert=False)
   return ""
