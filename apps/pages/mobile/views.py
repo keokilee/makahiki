1c4cf2fe6105fefd5039c75279fe19ee85b608d7
epy00n <epy00n@epy00n-laptop.(none)>
2011-07-18 01:42:32 -1000

    *moved the scoreboard's overall kwh to a separate file as this seems to be the source of the problem

--- apps/pages/mobile/views.py
+++ apps/pages/mobile/views.py
@@ -697,3 +697,7 @@ def profile(request):
       "count": range(0, 3),
     }
   }, context_instance=RequestContext(request))
+
+@login_required
+def overallkwh(request):
+  return render_to_response("mobile/scoreboard/overallkwh.html", {}, context_instance=RequestContext(request))
