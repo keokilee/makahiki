6facef596d8088f272568dddc903731ed0ca56ef
George Lee <keoki.lee@gmail.com>
2011-08-04 14:12:30 -1000

    Fixed multiple event triggering by unbinding and rebinding the click event in the document ready block.

--- media/js/makahiki.js
+++ media/js/makahiki.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($) {
 // Function to handle AJAX post requests.
 jQuery.ajaxSetup({ 
      beforeSend: function(xhr, settings) {
-         function getCookie(name) {
+         function getAjaxCookie(name) {
              var cookieValue = null;
              if (document.cookie && document.cookie != '') {
                  var cookies = document.cookie.split(';');
@@ -44,7 +44,7 @@ jQuery.ajaxSetup({
          }
          if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
              // Only send the token to relative URLs i.e. locally.
-             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+             xhr.setRequestHeader("X-CSRFToken", getAjaxCookie('csrftoken'));
          }
      } 
 });
