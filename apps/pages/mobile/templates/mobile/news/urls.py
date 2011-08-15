c70adabfec7e26819c0f9dfa91bf125c79d0a79c
epy00n <epy00n@epy00n-laptop.(none)>
2011-08-14 23:19:58 -1000

    revamped the newsfeed, hopefully its faster now...

--- apps/pages/mobile/templates/mobile/news/urls.py
+++ apps/pages/mobile/templates/mobile/news/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^$', 'views.news', name='news_index') 
+    url(r'^$', 'views.news', name='news_index'),
+    url(r'^(\d+)/?$', 'views.more_posts', name='news_more')
 )
