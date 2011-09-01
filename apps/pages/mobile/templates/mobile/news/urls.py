0aa4795f59b1d4cfaef22c7e23b654228209252f
Greg Burgess <epy00n@epy00n-laptop.(none)>
2011-08-31 18:03:58 -1000

    refactored help pages, and got events dialog to render

--- apps/pages/mobile/templates/mobile/news/urls.py
+++ apps/pages/mobile/templates/mobile/news/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'views.news', name='news_index'),
-    url(r'^(\d+)/?$', 'views.more_posts', name='news_more')
+    url(r'^(\d+)/?$', 'views.more_posts', name='news_more'),
+    url(r'^post/$', 'pages.news.views.post', name="news_post"),
 )
