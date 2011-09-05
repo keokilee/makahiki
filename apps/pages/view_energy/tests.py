5a7f4104b1ba134828161c2d8a770dd90124fc84
George Lee <keoki.lee@gmail.com>
2011-09-04 16:22:46 -1000

    Changed decimal numbers to strings because of Python 2.6's inability to convert float numbers to decimal.

--- apps/pages/view_energy/tests.py
+++ apps/pages/view_energy/tests.py
@@ -33,8 +33,8 @@ class EnergyFunctionalTestCase(TestCase):
       
     goal = FloorEnergyGoal.objects.create(
         floor=self.floor,
-        goal_usage=1.0,
-        actual_usage=2.0,
+        goal_usage="1.0",
+        actual_usage="2.0",
     )
     
     response = self.client.get(reverse("energy_index"))
@@ -44,8 +44,8 @@ class EnergyFunctionalTestCase(TestCase):
       
     goal = FloorEnergyGoal.objects.create(
         floor=self.floor,
-        goal_usage=1.0,
-        actual_usage=0.5,
+        goal_usage="1.0",
+        actual_usage="0.5",
     )
 
     response = self.client.get(reverse("energy_index"))
