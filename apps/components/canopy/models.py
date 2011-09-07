467aed20ad563b26d00500b3889db567388e7a42
George Lee <keoki.lee@gmail.com>
2011-09-06 18:29:02 -1000

    #405: Implemented mission completion by using a Django post_save signal to watch ActivityMember.

--- apps/components/canopy/models.py
+++ apps/components/canopy/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-from components.activities.models import ActivityBase
+from components.activities.models import ActivityBase, ActivityMember
 
 # Create your models here.
 class Mission(models.Model):
@@ -23,6 +25,45 @@ class MissionMember(models.Model):
   created_at = models.DateTimeField(auto_now_add=True, editable=False)
   updated_at = models.DateTimeField(auto_now=True, editable=False)
   
+  class Meta:
+    unique_together = ("user", "mission")
+    
+  def check_completed(self):
+    for activity in self.mission.activities.all():
+      try:
+        member = ActivityMember.objects.get(
+            user=self.user,
+            activity=activity,
+            approval_status="approved"
+        )
+      except ActivityMember.DoesNotExist:
+        return False
+        
+    return True
+  
+@receiver(post_save, sender=ActivityMember)
+def mission_activity_handler(sender, instance=None, **kwargs):
+  """
+  Signal handler for ActivityMembers to see if we completed any missions.
+  This is to be triggered when the ActivityMember finishes it's save.
+  """
+  # Check if we need to handle the instance.
+  if not instance:
+    return
+  if not instance.approval_status == "approved":
+    return
+    
+  members = MissionMember.objects.filter(
+      mission__activities__pk=instance.activity.id,
+      user=instance.user,
+      completed=False,
+  )
+  if members.count() > 0:
+    for member in members:
+      if member.check_completed():
+        member.completed = True
+        member.save()
+      
 class Post(models.Model):
   user = models.ForeignKey(User, related_name="canopyposts")
   text = models.TextField()
--- apps/components/canopy/tests.py
+++ apps/components/canopy/tests.py
@@ -1,16 +1,52 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+import datetime
 
 from django.test import TestCase
+from django.contrib.auth.models import User
 
+from components.canopy.models import Mission, MissionMember
+from components.activities.models import Activity, ActivityMember
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class MissionTest(TestCase):
+    def testMissionCompletion(self):
+      """
+      Test that a mission is completed when its related activity is completed.
+      """
+      user = User.objects.create_user("user", "user@test.com")
+      
+      activity = Activity.objects.create(
+          title="Test activity",
+          slug="test-activity",
+          description="Testing!",
+          duration=10,
+          point_value=10,
+          pub_date=datetime.datetime.today(),
+          expire_date=datetime.datetime.today() + datetime.timedelta(days=7),
+          confirm_type="text",
+          type="activity",
+      )
+      
+      mission = Mission.objects.create(
+          name="Test mission",
+          slug="test-mission",
+          description="A test mission",
+      )
+      mission.activities.add(activity)
+      
+      mission_member = MissionMember.objects.create(
+          user=user,
+          mission=mission,
+      )
+      
+      activity_member = ActivityMember(
+          user=user,
+          activity=activity,
+          approval_status="pending"
+      )
+      activity_member.save()
+      
+      self.assertFalse(mission_member.completed, "User should not have completed this mission.")
+      
+      activity_member.approval_status = "approved"
+      activity_member.save()
+      mission_member = MissionMember.objects.get(user=user, mission=mission)
+      self.assertTrue(mission_member.completed, "User should have completed this mission.")
