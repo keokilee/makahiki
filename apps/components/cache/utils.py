9cfb14e8b4e6608053f44b030ac7127e9c879ad5
George Lee <keoki.lee@gmail.com>
2011-09-12 15:59:34 -1000

    Created new module for invalidating template caches.

---
+++ apps/components/cache/utils.py
@@ -0,0 +1,30 @@
+from django.core.cache import cache
+from django.utils.hashcompat import md5_constructor
+from django.utils.http import urlquote
+
+def invalidate_template_cache(fragment_name, *variables):
+  """
+  Takes the name of the cache as well as additional arguments to invalidate the 
+  cache for the template.
+  
+  Credit to http://djangosnippets.org/snippets/1593/
+  """
+  args = md5_constructor(u':'.join([urlquote(var) for var in variables]))
+  cache_key = 'template.cache.%s.%s' % (fragment_name, args.hexdigest())
+  cache.delete(cache_key)
+  
+def invalidate_info_bar_cache(user):
+  """
+  Invalidates the user and lounge caches of the info bar.
+  """
+  invalidate_template_cache("infobar", user.username)
+  floor = user.get_profile().floor
+  if floor:
+    invalidate_template_cache("infobar", floor.dorm.name, floor.number)
+    
+def invalidate_smart_grid_cache(user):
+  """
+  Invalidates the smart grid's cache for a user.
+  """
+  invalidate_template_cache("smartgrid", user.username)
+  
