4697e307cb9696e10790f8f16a598da5c7728bd0
George Lee <keoki.lee@gmail.com>
2011-09-30 12:32:42 -1000

    First go at making popular activities and commitments.

--- apps/pages/status/views.py
+++ apps/pages/status/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
 from django.conf import settings
 
-from components.activities import get_popular_tasks
+from components.activities import get_popular_activities, get_popular_commitments
+from components.activities.models import ActivityBase
 from components.floors.models import Floor
 from components.makahiki_base import get_current_round
 from components.makahiki_profiles.models import Profile, ScoreboardEntry
@@ -69,7 +70,18 @@ def prizes(request):
     
 @user_passes_test(lambda u: u.is_staff, login_url="/account/cas/login")
 def popular_activities(request):
-  return render_to_response("status/activities.html", {}, context_instance=RequestContext(request))
+  tasks = {}
+  types = ActivityBase.objects.values('type').distinct()
+  for item in types:
+    task_type = item["type"]
+    if task_type == 'commitment':
+      tasks[task_type] = get_popular_commitments()
+    else:
+      tasks[task_type] = get_popular_activities(activity_type=task_type)
+  
+  return render_to_response("status/activities.html", {
+      "tasks": tasks,
+  }, context_instance=RequestContext(request))
         
 @user_passes_test(lambda u: u.is_staff, login_url="/account/cas/login")
 def event_rsvps(request):
