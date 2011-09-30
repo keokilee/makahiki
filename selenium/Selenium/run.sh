3b2918ed85d2685721882c4861ce9aeef0fe5e52
Greg Burgess <epy00n@epy00n-laptop.(none)>
2011-09-29 14:22:21 -1000

    made changes to runscript

--- selenium/Selenium/run.sh
+++ selenium/Selenium/run.sh
@@ -1,5 +1,24 @@
-
-python first_login.py
-python test_activity.py
-python add_remove_raffle.py
-python test_commitment.py
+echo ===COPYING FIXTURES...===
+cp fixtures/first_login/*.json fixtures 
+----------------------------------------------------------------------
+echo ===LOADING FIXTURES...===
+./scripts/load_data.sh
+----------------------------------------------------------------------
+echo ===RESETTING TESTBOT...===
+python manage.py reset_user_no_prompt testbot
+----------------------------------------------------------------------
+echo ===RESETTING TESTBOT PASSWORD...===
+python selenium/Selenium/reset_testbot.py
+echo ----------------------------------------------------------------------
+echo ===TESTING FIRST LOGIN PROCESS...===
+python selenium/Selenium/first_login.py
+echo ----------------------------------------------------------------------
+echo ===TESTING ACTIVITY...===
+python selenium/Selenium/test_activity.py
+echo ----------------------------------------------------------------------
+echo ===TESTING COMMITMENT...===
+python selenium/Selenium/test_commitment.py 
+echo ----------------------------------------------------------------------
+echo ===TESTING RAFFLE...===
+python selenium/Selenium/add_remove_raffle.py
+echo ----------------------------------------------------------------------
