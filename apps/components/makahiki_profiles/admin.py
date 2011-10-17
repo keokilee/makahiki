15b21609b7c9a9ee969d3fe84dcb5d548fbf9b1d
George Lee <keoki.lee@gmail.com>
2011-10-16 15:32:33 -1000

    Adding list display fields to profile so that we can sort through them.

--- apps/components/makahiki_profiles/admin.py
+++ apps/components/makahiki_profiles/admin.py
@@ -3,5 +3,6 @@ from components.makahiki_profiles.models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
   search_fields = ["user__username", "user__email"]
+  list_display = ['name', 'last_name', 'first_name',]
   
 admin.site.register(Profile, ProfileAdmin)
