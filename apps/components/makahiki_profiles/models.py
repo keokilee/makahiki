4bc76a08bda771b166ffe54556f137b4ff53ce4c
George Lee <keoki.lee@gmail.com>
2011-10-21 14:13:35 -1000

    Migration for Profiles to support referrers.
    Added two new columns to the profiles table: referrer_email and referrer_awarded.

--- apps/components/makahiki_profiles/models.py
+++ apps/components/makahiki_profiles/models.py
@@ -116,6 +116,10 @@ class Profile(models.Model):
   daily_visit_count = models.IntegerField(default=0, editable=False)
   last_visit_date = models.DateField(null=True, blank=True)
   
+  # Check for referrer
+  referrer_email = models.EmailField(null=True, blank=True)
+  referrer_awarded = models.BooleanField(default=False)
+  
   def __unicode__(self):
       return self.name
   
