45ff7a0c0bdabe2d2e0acc155ff7c962029b5804
George Lee <keoki.lee@gmail.com>
2011-11-21 17:51:25 -1000

    Added ability to track sub page visits.

--- time_spent.py
+++ time_spent.py
@@ -41,37 +41,94 @@ def calculate_user_time():
     for user in users:
         logs = MakahikiLog.objects.filter(remote_user=user.username).order_by('request_time')
         sys.stdout.write('Processing %d logs for %s (%d/%d)\n' % (logs.count(), user.username, counter, user_count))
-        # Iterate over the logs and track previous time and time spent.
-        prev = datetime.datetime.strptime(logs[0].request_time, "%Y-%m-%d %H:%M:%S")
-        cur_session = total = 0
-        for log in logs[1:]:
-            current = datetime.datetime.strptime(log.request_time, "%Y-%m-%d %H:%M:%S")
-            diff = current - prev
-            # Start a new interval if 30 minutes have passed.
-            if diff.total_seconds() > (60 * 30):
-                if cur_session == 0:
-                    total += 60 # Assume they spent a minute viewing a single page.
-                else:
-                    total += cur_session
-                cur_session = 0
-            else:
-                cur_session += diff.total_seconds()
-                
-            prev = current
-            
-        # Append any session that was in progress.
-        total += cur_session
-        users_file.write('%s,%d\n' % (user.username, total))
+        total = _time_spent(logs)
+        activity_time = _page_time('/activities', logs)
+        energy_time = _page_time('/energy', logs)
+        prize_time = _page_time('/prizes', logs)
+        
+        users_file.write('%s,%d,%d,%d,%d\n' % (user.username, total, activity_time, energy_time, prize_time))
         lounge = str(user.get_profile().floor)
-        lounges[lounge] = lounges.get(lounge, 0) + total
+        lounges[lounge] = lounges.get(lounge, {})
+        lounges[lounge]['total'] = lounges[lounge].get('total', 0) + total
+        lounges[lounge]['activities'] = lounges[lounge].get('activities', 0) + activity_time
+        lounges[lounge]['energy'] = lounges[lounge].get('energy', 0) + energy_time
+        lounges[lounge]['prizes'] = lounges[lounge].get('prizes', 0) + prize_time
         counter += 1
         
-    for lounge, time in lounges.iteritems():
-        lounge_file.write('%s,%d\n' % (lounge, time))
+    for lounge, items in lounges.iteritems():
+        lounge_file.write('%s,%d,%d,%d,%d\n' % (lounge, items['total'], items['activities'], items['energy'], items['prizes']))
         
     lounge_file.close()
     users_file.close()
     
+def _time_spent(logs):
+    # Iterate over the logs and track previous time and time spent.
+    prev = datetime.datetime.strptime(logs[0].request_time, "%Y-%m-%d %H:%M:%S")
+    cur_session = total = 0
+    
+    for log in logs[1:]:
+        current = datetime.datetime.strptime(log.request_time, "%Y-%m-%d %H:%M:%S")
+        diff = current - prev
+        # Start a new interval if 30 minutes have passed.
+        if diff.total_seconds() > (60 * 30):
+            if cur_session == 0:
+                total += 60 # Assume they spent a minute viewing a single page.
+            else:
+                total += cur_session
+            cur_session = 0
+        else:
+            cur_session += diff.total_seconds()
+            
+        prev = current
+        
+    # Append any session that was in progress.
+    total += cur_session
+    return total
+    
+def _page_time(url_prefix, logs):
+    start = prev = None
+    total = cur_session = 0
+    for log in logs:
+        current = datetime.datetime.strptime(log.request_time, "%Y-%m-%d %H:%M:%S")
+        diff = current - prev if prev else None
+        # Check if we are starting a session
+        if log.url.startswith(url_prefix) and not start:
+            start = current
+            cur_session = 0
+            
+        # Check if we are within a session.
+        elif start and prev:
+            diff = current - prev
+            if log.url.startswith(url_prefix) and diff.total_seconds() < (60 * 30):
+                # Append to current session.
+                cur_session += diff.total_seconds()
+            elif diff.total_seconds() >= (60 * 30):
+                # Break in session.  Close the previous and start a new one.
+                if cur_session == 0:
+                    # Assume user spent a minute.
+                    cur_session = 60
+                    
+                total += cur_session
+                cur_session = 0
+                if log.url.startswith(url_prefix):
+                    start = current
+                else:
+                    start = None
+            else:
+                # Person navigated away.  Close the session.
+                cur_session += diff.total_seconds()
+                if cur_session == 0:
+                    # Assume user spent a minute.
+                    cur_session = 60
+                    
+                total += cur_session
+                cur_session = 0
+                start = None
+                
+        prev = current 
+        
+    return total
+        
 def calculate_admin_time():
     admin_file = open('time_spent_admin.csv', 'w')
     users = User.objects.filter(is_staff=True)
@@ -107,4 +164,3 @@ def calculate_admin_time():
 if __name__ == "__main__":
     calculate_user_time()
     calculate_admin_time()
-    calculate_admin_answers()
