349fe96280286771801eeda89ac8c0623d7483b0
George Lee <keoki.lee@gmail.com>
2011-12-16 12:05:37 -1000

    Updating settings.

--- makahiki/manage.py
+++ makahiki/manage.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python
-import sys
-
-from os.path import abspath, dirname, join
-
-# try:
-#     import pinax
-# except ImportError:
-#     sys.stderr.write("Error: Can't import Pinax. Make sure you have it installed or use pinax-boot.py to properly create a virtual environment.")
-#     sys.exit(1)
-
-from django.conf import settings
-from django.core.management import setup_environ, execute_from_command_line
-
+from django.core.management import execute_manager
+import imp, sys
 try:
-    import settings as settings_mod # Assumed to be in the same directory.
+    imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    import sys
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
 
-# setup the environment before we start accessing things in the settings.
-setup_environ(settings_mod)
-
-# sys.path.insert(0, join(settings.PINAX_ROOT, "apps"))
 sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
+import settings
 
 if __name__ == "__main__":
-    execute_from_command_line()
+    execute_manager(settings)
