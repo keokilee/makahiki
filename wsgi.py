35d882ff8b2f958a0b304635ee76cbda2e6be3f8
George Lee <keoki.lee@gmail.com>
2012-01-11 13:10:27 -1000

    Added additional configuration from pinax.wsgi to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 import os
+from os.path import abspath, dirname, join
 import sys
+
 os.environ['DJANGO_SETTINGS_MODULE']  = 'makahiki.settings'
+sys.path.insert(0, abspath(join(dirname(__file__), "../")))
 sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
