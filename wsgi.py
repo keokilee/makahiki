ccf86afa633b46f50b15fd1e036095ec0dd4ecf6
George Lee <keoki.lee@gmail.com>
2012-01-11 13:02:48 -1000

    Added additional wsgi commands.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 import os
+import sys
 os.environ['DJANGO_SETTINGS_MODULE']  = 'makahiki.settings'
+sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
-
