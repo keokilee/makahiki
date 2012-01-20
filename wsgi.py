9982fa2b95a37050e85d6a80e914380dcba3561c
George Lee <keoki.lee@gmail.com>
2012-01-19 16:15:19 -1000

    Updated wsgi.py because of error.

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,9 @@ import sys
 
 os.environ['DJANGO_SETTINGS_MODULE']  = 'makahiki.settings'
 sys.path.insert(0, abspath(join(dirname(__file__), "../")))
+
+from django.conf import settings
+
 sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
 from django.core.handlers.wsgi import WSGIHandler
 application = WSGIHandler()
